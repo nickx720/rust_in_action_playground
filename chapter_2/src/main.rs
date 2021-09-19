dca359ca6e88ef068df3e6a91d8ae95e6e8e3553
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-09-19 07:50:01 -0400

    iterators intro

--- chapter_2/src/main.rs
+++ chapter_2/src/main.rs
@@ -3,6 +3,18 @@ mod complex;
 mod mandelbrot;
 use mandelbrot::{calculate_mandelbrot,render_mandelbrot};
 fn main() {
-    let mandelbrot = calculate_mandelbrot(1000, -2.0, 1.0, -1.0, 1.0, 80, 24);
-    render_mandelbrot(mandelbrot);
+//    let mandelbrot = calculate_mandelbrot(1000, -2.0, 1.0, -1.0, 1.0, 80, 24);
+//    render_mandelbrot(mandelbrot);
+let search_term = "picture";
+let quote = "\
+    Every face, every shop, bedroom, window, public-house
+    and dark square is a picture feverishly turned-- in search of
+    what ? It is the same with books. What do we seek through millions of pages?";
+
+
+for (line_num,line) in quote.lines().enumerate(){
+    if line.contains(search_term){
+        println!("{}: {}",line_num,line);
+    }
+}
 }
