83d4fc27ad1f69c1f50921917c90d38eec3fa093
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-09-26 13:25:36 -0400

    Completed Part 2

--- chapter_2/src/main.rs
+++ chapter_2/src/main.rs
@@ -2,54 +2,55 @@ mod abs;
 mod complex;
 mod mandelbrot;
 mod quote;
+mod needle;
+
+use std::fs::File;
+use std::io;
+use std::io::BufReader; // buffered I/O which reduces system calls to IO
+use std::io::prelude::*;
+use regex::Regex;
+use clap::{App,Arg};
 use mandelbrot::{calculate_mandelbrot,render_mandelbrot};
-fn main() {
-//    let mandelbrot = calculate_mandelbrot(1000, -2.0, 1.0, -1.0, 1.0, 80, 24);
-//    render_mandelbrot(mandelbrot);
-let ctx_lines = 2;
-let needle = "oo";
-let haystack = "\
-Every face, every shop,
-bedroom window, public-house, and
-dark square is a picture
-feverishly turned- - in search of what?
-It is the same with books.
-What do we seek
-through millions of pages?";
 
-let mut tags: Vec<usize> = vec![];
-let mut ctx: Vec<Vec<(usize,String)>> = vec![];
-
-for (i,line) in haystack.lines().enumerate() {
-    if line.contains(needle) {
-        tags.push(i);
-
-        let v = Vec::with_capacity(2* ctx_lines + 1);
-        ctx.push(v);
-    }
-}
-
-if tags.is_empty() {
-    return;
-}
-
-for (i,line) in haystack.lines().enumerate(){
-    for (j,tag) in tags.iter().enumerate(){
-        let lower_bound = tag.saturating_sub(ctx_lines);
-        let upper_bound = tag + ctx_lines;
-
-        if(i >= lower_bound) && (i <= upper_bound) {
-            let line_as_string = String::from(line);
-            let local_ctx = (i, line_as_string);
-            ctx[j].push(local_ctx);
+fn process_lines<T:BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines(){
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}",line),
+            None => (),
         }
     }
 }
 
-for local_ctx in ctx.iter(){
-    for &(i, ref line) in local_ctx.iter(){
-        let line_num = i + 1;
-        println!("{}: {}",line_num,line);
-    }
+fn main() {
+//    let mandelbrot = calculate_mandelbrot(1000, -2.0, 1.0, -1.0, 1.0, 80, 24);
+//    render_mandelbrot(mandelbrot);
+let args = App::new("grep-lite")
+    .version("0.1")
+    .about("searches for patterns")
+    .arg(Arg::with_name("pattern")
+        .help("The pattern to search for")
+        .takes_value(true)
+        .required(true))
+    .arg(Arg::with_name("input")
+        .help("File to search")
+        .takes_value(true)
+        .required(false))
+    .get_matches();
+
+let pattern = args.value_of("pattern").unwrap();
+let re = Regex::new(pattern).unwrap();
+
+let input = args.value_of("input").unwrap_or("-");
+
+if input == "-" {
+    let stdin = io::stdin();
+    let reader = stdin.lock();
+    process_lines(reader, re);
+} else {
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
+    process_lines(reader, re);
 }
+
 }
