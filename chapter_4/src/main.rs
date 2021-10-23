dda822b4a6295be31e54daaecd69e858240ec58c
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-10-23 05:29:25 -0400

    manual clone and copy

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -10,6 +10,24 @@ enum StatusMessage {
     Ok,
 }
 
+impl Copy for CubeSat {}
+
+impl Copy for StatusMessage {}
+
+impl Clone for CubeSat{
+    fn clone(&self)-> Self{
+        CubeSat{
+            id: self.id
+        }
+    }
+}
+
+impl Clone for StatusMessage {
+    fn clone(&self)-> Self{
+        *self
+    }
+}
+
 #[derive(Debug)]
 struct Mailbox {
     messages: Vec<Message>,
