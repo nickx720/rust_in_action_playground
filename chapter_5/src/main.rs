1d1858fb32163bf1e4aa2a2408e0d4e5b986a2f1
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-11-07 07:42:17 -0500

    decoding exponent

--- chapter_5/src/main.rs
+++ chapter_5/src/main.rs
@@ -9,10 +9,20 @@ fn isolating_sign_bit(){
     dbg!(format!("{:#b}",42));
 }
 
+fn isolating_the_exponent(){
+    let n: f32 = 42.42;
+    let n_bits: u32 = n.to_bits();
+    let exponent_ = n_bits >> 23;
+    let exponent_ = exponent_ & 0xff;
+    let exponent = (exponent_ as i32) - 12;
+    dbg!(exponent);
+}
+
 fn main() {
     //    same();
     //    interpret_float_string_as_integer();
  //   bit_patterns_translate_to_a_fixed_number_of_integers()
 // understanding_endianess()
-isolating_sign_bit()
+//isolating_sign_bit()
+isolating_the_exponent()
 }
