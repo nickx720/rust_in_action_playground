290cdcfc8c76830388bdbea29055ac6939493045
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-11-21 07:00:07 -0500

    started with chip8

---
+++ chapter_5/src/chip8/CPU.rs
@@ -0,0 +1,4 @@
+pub struct CPU {
+    current_operation: u16,
+    registers: [u8; 2],
+}
--- chapter_5/src/main.rs
+++ chapter_5/src/main.rs
@@ -1,79 +1,16 @@
+mod chip8;
 mod foo;
 mod mantisa;
+mod q7;
 use foo::understanding_endianess;
 use mantisa::mantisaa_main;
 
-// 7 bits with one sign bit
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Q7(i8);
-
-impl From<f64> for Q7 {
-    fn from(n: f64) -> Self {
-        // assert!(n >= -1.0)
-        // assert!(n<= 1.0);
-        if n >= 1.0 {
-            Q7(127)
-        } else if n <= -1.0 {
-            Q7(-128)
-        } else {
-            Q7((n * 128.0) as i8)
-        }
-    }
-}
-
-impl From<Q7> for f64 {
-    fn from(n: Q7) -> f64 {
-        (n.0 as f64) * 2_f64.powf(-7.0)
-    }
-}
-
-impl From<f32> for Q7 {
-    fn from(n: f32) -> Self {
-        Q7::from(n as f64)
-    }
-}
-
-impl From<Q7> for f32 {
-    fn from(n: Q7) -> f32 {
-        f64::from(n) as f32
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn out_of_bounds() {
-        assert_eq!(Q7::from(10.), Q7::from(1.));
-        assert_eq!(Q7::from(-10.), Q7::from(-1.));
-    }
-
-    #[test]
-    fn f32_to_q7() {
-        let n1: f32 = 0.7;
-        let q1 = Q7::from(n1);
-
-        let n2 = -0.4;
-        let q2 = Q7::from(n2);
-
-        let n3 = 123.0;
-        let q3 = Q7::from(n3);
-
-        assert_eq!(q1, Q7(89));
-        assert_eq!(q2, Q7(-51));
-        assert_eq!(q3, Q7(127));
-    }
-
-    #[test]
-    fn q7_to_f32() {
-        let q1 = Q7::from(0.7);
-        let n1 = f32::from(q1);
-        assert_eq!(n1, 0.6953125);
-
-        let q2 = Q7::from(n1);
-        let n2 = f32::from(q2);
-        assert_eq!(n1, n2);
-    }
+fn mock_rand(n: u8) -> f32 {
+    let base: u32 = 0b0_01111110_00000000000000000000000;
+    let large_n = (n as u32) << 15;
+    let f32_bits = base | large_n;
+    let m = f32::from_bits(f32_bits);
+    2.0 * (m - 0.5)
 }
 
 fn main() {
@@ -83,4 +20,7 @@ fn main() {
     // understanding_endianess()
     //isolating_sign_bit()
     // mantisaa_main()
+    println!("max of input range: {:08b} -> {:?}", 0xff, mock_rand(0xff));
+    println!("mid of input range: {:08b} -> {:?}", 0x7f, mock_rand(0x7f));
+    println!("min of input range: {:08b} -> {:?}", 0x00, mock_rand(0x00));
 }
