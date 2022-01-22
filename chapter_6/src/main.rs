caa23842cf29d1fc9db0aa405b4d9963b17f271d
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-01-22 10:43:06 -0500

    raw pointers

--- chapter_6/src/main.rs
+++ chapter_6/src/main.rs
@@ -1,8 +1,24 @@
-static B: [u8; 10] = [99, 97, 114, 114, 121, 116, 111, 119, 101, 108];
-static C: [u8; 11] = [116, 104, 97, 110, 107, 115, 102, 105, 115, 104, 0];
+mod references;
+use references::{references, B, C};
+
+use std::borrow::Cow;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
 fn main() {
+    //    references();
     let a = 42;
-    let b = &B;
-    let c = &C;
-    println!("a: {}, b: {:p}, c: {:p}", a, b, c);
+    let b: String;
+    let c: Cow<str>;
+
+    unsafe {
+        let b_ptr = &B as *const u8 as *mut u8;
+        b = String::from_raw_parts(b_ptr, 10, 10);
+
+        let c_ptr = &C as *const u8 as *const c_char;
+
+        c = CStr::from_ptr(c_ptr).to_string_lossy();
+    }
+
+    println!("a : {}, b: {}, c: {}", a, b, c);
 }
