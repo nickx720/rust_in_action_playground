0f6b083b8cc6fdc584a00236749d92431808a938
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-01-30 06:25:42 -0500

    how to use references and drop

--- chapter_6/src/stack.rs
+++ chapter_6/src/stack.rs
@@ -1,3 +1,4 @@
+use std::mem::drop;
 // Implicit conversion
 fn is_strong<T: Into<String>>(password: T) -> bool {
     password.into().len() > 5
@@ -12,4 +13,14 @@ pub fn stack_main() {
     let pw = "justok";
     let is_strong = is_strong_explicit(pw);
     dbg!("The output is {:?}", is_strong);
+    let a = Box::new(1);
+    let b = Box::new(1);
+    let c = Box::new(1);
+
+    // assinging to stack
+    let result_1 = *a + *b + *c;
+    drop(a);
+    let d = Box::new(2);
+    let result_2 = *b + *c + *d;
+    println!("The output is {},{}", result_1, result_2);
 }
