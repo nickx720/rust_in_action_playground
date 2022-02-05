e76e21f0292eb6ee39eab94b4ab8b474328931f9
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-02-05 05:59:33 -0500

    particles

---
+++ chapter_6/src/unsafer.rs
@@ -0,0 +1,15 @@
+pub fn unsafe_test() {
+    let a: i64 = 42;
+    let a_ptr = &a as *const i64;
+    let a_addr: usize = unsafe { std::mem::transmute(a_ptr) };
+    println!("a: {} ({:p}...0x{:x})", a, a_ptr, a_addr + 7);
+    // println!("a : {} ({:p})", a, a_ptr);
+
+    // Unsafe pointer from integer
+    let ptr = 42 as *const Vec<String>;
+
+    unsafe {
+        let new_addr = ptr.offset(4);
+        println!("{:p} -> {:p}", ptr, new_addr);
+    }
+}
