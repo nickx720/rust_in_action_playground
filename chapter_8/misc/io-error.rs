14d795d2c6cc2fd5d24099dfc2424a0161bf2b97
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-03-26 11:04:09 -0400

    Making the errors more explicit

--- chapter_8/misc/io-error.rs
+++ chapter_8/misc/io-error.rs
@@ -1,11 +1,26 @@
 use std::error::Error;
+use std::fmt;
 use std::fs::File;
-use std::net::Ipv6Addr;
+use std::net::{AddrParseError, Ipv6Addr};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let _f = File::open("invisible.txt")?;
+#[derive(Debug)]
+enum UpstreamError {
+    IO(std::io::Error),
+    Parsing(AddrParseError),
+}
 
-    let _localhost = ":1".parse::<Ipv6Addr>()?;
+impl fmt::Display for UpstreamError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl Error for UpstreamError {}
+
+fn main() -> Result<(), UpstreamError> {
+    let _f = File::open("invisible.txt").map_err(UpstreamError::IO)?;
+
+    let _localhost = ":1".parse::<Ipv6Addr>().map_err(UpstreamError::Parsing)?;
 
     Ok(())
 }
