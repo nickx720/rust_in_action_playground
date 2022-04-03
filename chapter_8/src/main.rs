cd57a32c19247004a62756512869d79d9d84f7b9
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-04-03 05:22:59 -0400

    Mac address code

--- chapter_8/src/main.rs
+++ chapter_8/src/main.rs
@@ -1,12 +1,15 @@
 mod dns_resolver;
+mod mac_play;
 mod reqwest_play;
 mod rpg_play;
 use dns_resolver::main_dns;
+use mac_play::mac_main;
 use reqwest_play::reqwestmain;
 use rpg_play::rpgplaymain;
 
 fn main() {
     //    reqwestmain();
     //rpgplaymain();
-    dns_resolver::main_dns();
+    //dns_resolver::main_dns();
+    mac_main();
 }
