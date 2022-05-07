64b86613adb434ca2a0d36967f6b83013dd77d88
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-05-07 04:15:58 -0400

    Updating the clock

--- chapter_9/src/clock.rs
+++ chapter_9/src/clock.rs
@@ -1,6 +1,9 @@
 use chrono::DateTime;
 use chrono::Local;
+use chrono::TimeZone;
 use clap::{App, Arg};
+use libc;
+use std::mem::zeroed;
 
 struct Clock;
 
@@ -8,15 +11,30 @@ impl Clock {
     fn get() -> DateTime<Local> {
         Local::now()
     }
-    fn set() -> ! {
-        unimplemented!()
+    fn set<Tz: TimeZone>(t: DateTime<Tz>) -> () {
+        use libc::{settimeofday, timezone};
+        use libc::{suseconds_t, time_t, timeval};
+
+        let t = t.with_timezone(&Local);
+        let mut u: timeval = unsafe { zeroed() };
+        u.tv_sec = t.timestamp() as time_t;
+        u.tv_usec = t.timestamp_subsec_micros() as suseconds_t;
+        unsafe {
+            let mock_tz: *const timezone = std::ptr::null();
+            settimeofday(&u as *const timeval, mock_tz);
+        }
     }
 }
 
 pub fn clock_main() {
     let app = App::new("clock")
-        .version("0.1")
+        .version("0.1.2")
         .about("Gets and (aspirationally) sets the time.")
+        .after_help(
+            "Note: UNIX timestamps are parsed as whole \
+        seconds since 1st January 1970 0:00:00 UTC. \
+        For more accuracy. use another format.",
+        )
         .arg(
             Arg::with_name("action")
                 .takes_value(true)
@@ -40,7 +58,16 @@ pub fn clock_main() {
     let std = args.value_of("std").unwrap();
 
     if action == "set" {
-        unimplemented!()
+        let t_ = args.value_of("datetime").unwrap();
+
+        let parser = match std {
+            "rfc2822" => DateTime::parse_from_rfc2822,
+            "rfc3339" => DateTime::parse_from_rfc3339,
+            _ => unimplemented!(),
+        };
+
+        let err_msg = format!("Unable to parse {t_} according to {std}");
+        let t = parser(t_).expect(&err_msg);
     }
 
     let now = Clock::get();
