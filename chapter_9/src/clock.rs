537622ad67d18ce31dea64111da3d30ced94ac86
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-05-21 03:29:41 -0400

    main pending

--- chapter_9/src/clock.rs
+++ chapter_9/src/clock.rs
@@ -98,6 +98,92 @@ impl NTPMessage {
     }
 
     // start at tx_time
+    fn tx_time(&self) -> Result<NTPTimestamp, std::io::Error> {
+        self.parse_timestamp(40)
+    }
+}
+fn weighted_mean(values: &[f64], weights: &[f64]) -> f64 {
+    let mut result = 0.0;
+    let mut sum_of_weights = 0.0;
+
+    for (v, w) in values.iter().zip(weights) {
+        result += v * w;
+        sum_of_weights += w;
+    }
+    result
+}
+
+fn ntp_roundtrip(host: &str, port: u16) -> Result<NTPResult, std::io::Error> {
+    let destination = format!("{host} : {port}");
+    let timeout = Duration::from_secs(1);
+
+    let request = NTPMessage::client();
+    let mut response = NTPMessage::new();
+
+    let message = request.data;
+
+    let udp = UdpSocket::bind(LOCAL_ADDR)?;
+    udp.connect(&destination).expect("unable to connect");
+
+    let t1 = Utc::now();
+
+    udp.send(&message)?;
+    udp.set_read_timeout(Some(timeout))?;
+    udp.recv_from(&mut response.data)?;
+    let t4 = Utc::now();
+
+    let t2: DateTime<Utc> = response.rx_time().unwrap().into();
+
+    let t3: DateTime<Utc> = response.tx_time().unwrap().into();
+
+    Ok(NTPResult {
+        t1: t1,
+        t2: t2,
+        t3: t3,
+        t4: t4,
+    })
+}
+
+fn check_time() -> Result<f64, std::io::Error> {
+    const NTP_PORT: u16 = 123;
+
+    let servers = ["time.nost.gov", "time.apple.com", "time.euro.apple.com"];
+
+    let mut times = Vec::with_capacity(servers.len());
+
+    for &server in servers.iter() {
+        print!("{} =>", server);
+
+        let calc = ntp_roundtrip(&server, NTP_PORT);
+
+        match calc {
+            Ok(time) => {
+                println!(" {}ms away from local system time", time.offset());
+                times.push(time);
+            }
+            Err(_) => {
+                println!(" ? [response took so long]")
+            }
+        };
+    }
+
+    let mut offsets = Vec::with_capacity(servers.len());
+    let mut offset_weights = Vec::with_capacity(servers.len());
+
+    for time in &times {
+        let offset = time.offset() as f64;
+        let delay = time.delay() as f64;
+
+        let weight = 1_000_000.0 / (delay * delay);
+        if weight.is_finite() {
+            offsets.push(offset);
+            offset_weights.push(weight);
+        }
+    }
+
+    let avg_offset = weighted_mean(&offsets, &offset_weights);
+
+    Ok(avg_offset)
 }
 
 struct Clock;
