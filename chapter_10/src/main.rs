0534b45ae3feb6f3a4cefc14ec7dedb52f290f5d
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-05-29 06:07:42 -0400

    Suspend thread

--- chapter_10/src/main.rs
+++ chapter_10/src/main.rs
@@ -1,6 +1,9 @@
+mod loop_waiting;
+mod suspend_thread;
 mod thread_example;
 use thread_example::{thread_example_main, thread_two_threads_example};
 fn main() {
-    thread_example_main();
-    thread_two_threads_example();
+    //    thread_example_main();
+    //    thread_two_threads_example();
+    suspend_thread::suspend_thread_main();
 }
