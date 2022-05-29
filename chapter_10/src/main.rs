8324fba3c628bf5f35b6807375c63b4dde84eb3e
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-05-29 06:13:38 -0400

    Loop waiting test

--- chapter_10/src/main.rs
+++ chapter_10/src/main.rs
@@ -5,5 +5,6 @@ use thread_example::{thread_example_main, thread_two_threads_example};
 fn main() {
     //    thread_example_main();
     //    thread_two_threads_example();
-    suspend_thread::suspend_thread_main();
+    //    suspend_thread::suspend_thread_main();
+    loop_waiting::loop_waiting_main();
 }
