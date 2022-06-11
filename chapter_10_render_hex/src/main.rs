7ed67b24b6d91a9a23b27673332b10d4feede403
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-06-11 08:07:15 -0400

    wip render hex

--- chapter_10_render_hex/src/main.rs
+++ chapter_10_render_hex/src/main.rs
@@ -4,14 +4,86 @@ use svg::node::element::path::{Command,Data,Position};
 use svg::node::element::{Path,Rectangle};
 use svg::Document;
 
+use crate::Operation::{
+    Forward,
+    Home,
+    Noop,
+    TurnLeft,
+    TurnRight,
+};
+
+use crate::Orientation::{
+    East,
+    North,
+    South,
+    West
+};
+
+const WIDTH: isize = 400;
+const HEIGHT: isize = WIDTH;
+
+const HOME_Y: isize = HEIGHT / 2;
+const HOME_X: isize = WIDTH / 2;
+
+const STROKE_WIDTH: usize = 5;
+
+#[derive(Debug,Clone,Copy)]
+enum Orientation {
+    North,
+    East,
+    West,
+    South,
+}
+
 #[derive(Debug,Clone,Copy)]
 enum Operation{
     Forward(isize),
     TurnLeft,
     TurnRight,
     Home,
-    Noop(usize),
+    Noop(u8),
 }
+
+#[derive(Debug)]
+struct Artist {
+    x: isize,
+    y: isize,
+    heading: Orientation,
+}
+
+impl Artist {
+    fn new() -> Artist {
+        Artist{
+            heading: North,
+            x: HOME_X,
+            y: HOME_Y,
+        }
+    }
+
+    fn home(&mut self){
+        self.x = HOME_X;
+        self.y = HOME_Y;
+    }
+
+    fn forward(&mut self, distance: isize) {
+        match self.heading{
+            North => self.y += distance,
+            South => self.y -= distance,
+            West => self.x +=distance,
+            East => self.x -= distance,
+        }
+    }
+
+    fn turn_right(&mut self){
+        self.heading = match self.heading {
+            North => East,
+            South => West,
+            West => North,
+            East =>South,
+        }
+    }
+}
+
 
 fn parse(input:&str) -> Vec<Operation>{
     let mut steps = Vec::<Operation>::new();
