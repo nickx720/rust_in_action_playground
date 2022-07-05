3b39878da7b01dd235dfb04d619a413696b0fbdf
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-07-04 20:19:20 -0400

    Changes

--- chapter_10_render_hex/src/channels_intro.rs
+++ chapter_10_render_hex/src/channels_intro.rs
@@ -8,6 +8,31 @@ enum ConnectivityCheck {
     Pang,
 }
 
+pub fn sending_msg_spawned_channel() {
+    let n_messages = 3;
+    let (requests_tx, requests_rx) = unbounded();
+    let (responses_tx, responses_rx) = unbounded();
+
+    thread::spawn(move || loop {
+        match requests_rx.recv().unwrap() {
+            Pong => eprintln!("unexpected pong response"),
+            Ping => responses_tx.send(ConnectivityCheck::Pong).unwrap(),
+            Pang => return,
+        }
+    });
+
+    for _ in 0..n_messages {
+        requests_tx.send(ConnectivityCheck::Ping).unwrap();
+    }
+    requests_tx.send(ConnectivityCheck::Pang).unwrap();
+
+    for _ in 0..n_messages {
+        select! {
+            recv(responses_rx) -> msg => println!("{:?}",msg),
+        }
+    }
+}
+
 pub fn channels_intro_main() {
     let (tx, rx) = unbounded();
 
