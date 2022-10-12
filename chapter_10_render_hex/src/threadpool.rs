cd089b5583351d82f776ee0664c621ba96326a2b
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-10-12 03:38:45 -0400

    wip

--- chapter_10_render_hex/src/threadpool.rs
+++ chapter_10_render_hex/src/threadpool.rs
@@ -1,6 +1,24 @@
 use std::env;
 use std::thread;
 
+enum Work {
+    Task((usize, u8)),
+    Finished,
+}
+
+fn parse_byte(byte: u8) -> Operation {
+    match byte {
+        b'0' => Home,
+        b'1'..=b'9' => {
+            let distance = (byte - 0x30) as isize;
+            Forward(distance * (HEIGHT / 10))
+        }
+        b'a' | b'b' | b'c' => TurnLeft,
+        b'd' | b'e' | b'f' => TurnRight,
+        _ => Noop(byte),
+    }
+}
+
 use crossbeam::channel::unbounded;
 pub fn threadpool_main() {
     todo!()
