36ceb5766405dfa267f2abcf00f211c58bfad7ae
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-10-13 03:38:15 -0400

    threads

--- chapter_10_render_hex/src/threadpool.rs
+++ chapter_10_render_hex/src/threadpool.rs
@@ -1,6 +1,6 @@
+use crossbeam::channel::unbounded;
 use std::env;
 use std::thread;
-
 enum Work {
     Task((usize, u8)),
     Finished,
@@ -19,7 +19,40 @@ fn parse_byte(byte: u8) -> Operation {
     }
 }
 
-use crossbeam::channel::unbounded;
+fn parse(input: &str) -> Vec<Operation> {
+    let n_threads = 2;
+    let (todo_tx, todo_rx) = unbounded();
+    let (results_tx, results_rx) = unbounded();
+    let mut n_bytes = 0;
+    for (i_byte) in input.bytes().enumerate() {
+        todo_tx.send(Work::Task(i, byte)).unwrap();
+        n_bytes += 1;
+    }
+    for _ in 0..n_threads {
+        todo_tx.send(Work::Finished).unwrap();
+    }
+
+    for _ in 0..n_threads {
+        let todo = todo_rx.clone();
+        let results = results_tx.clone();
+        thread::spawn(move || loop {
+            let task = todo.recv();
+            let result = match task {
+                Err(_) => break,
+                Ok(Work::Finished) => break,
+                Ok(Work::Task((i, byte))) => (i, parse_byte(byte)),
+            };
+            results.send(result).unwrap();
+        });
+    }
+    let mut ops = vec![Noop(0); n_bytes];
+    for _ in 0..n_bytes {
+        let (i, op) = results_rx.recv().unwrap();
+        ops[i] = op;
+    }
+    ops
+}
+
 pub fn threadpool_main() {
     todo!()
 }
