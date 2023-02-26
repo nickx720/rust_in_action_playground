803a333e40303ebcf08e72b9b24897f02a836d1a
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2023-02-26 09:54:37 -0500

    completed till fledge-os text input

--- chapter_11/fledge/ch11/ch11-fledgeos-0/src/main.rs
+++ chapter_11/fledge/ch11/ch11-fledgeos-0/src/main.rs
@@ -1,9 +1,12 @@
+#![feature(lang_items)]
 #![no_std] // <1>
 #![no_main] // <1>
 #![feature(core_intrinsics)] // <2>
 
 use core::intrinsics; // <2>
-use core::panic::PanicInfo; // <3>
+use core::panic::PanicInfo;
+
+use x86_64::instructions::hlt; // <3>
 
 #[panic_handler]
 #[no_mangle]
@@ -11,6 +14,9 @@ pub fn panic(_info: &PanicInfo) -> ! {
     intrinsics::abort(); // <4>
 }
 
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn eh_personality() {}
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let framebuffer = 0xb8000 as *mut u8;
