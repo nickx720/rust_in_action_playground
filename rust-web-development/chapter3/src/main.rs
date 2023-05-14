66ab6206c590529ff4070d9bed3cc0a86d12e858
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2023-05-14 11:19:59 -0400

    adding question

--- rust-web-development/chapter3/src/main.rs
+++ rust-web-development/chapter3/src/main.rs
@@ -1,4 +1,42 @@
+use std::{
+    io::{Error, ErrorKind},
+    str::FromStr,
+};
 use warp::Filter;
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let hello = warp::get().map(|| format!("Hello World"));
