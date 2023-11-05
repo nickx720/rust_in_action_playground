b33ace36178d73506fe3e3411d3ea681b61123df
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2023-11-05 05:39:05 -0600

    cleaning up

--- rust-web-development/markdown-html/src/lib/mod.rs
+++ rust-web-development/markdown-html/src/lib/mod.rs
@@ -1,8 +1,16 @@
+use std::fs;
+
 use markdown::to_html;
-pub fn runfromlib(path: String) {
-    let path = path.to_owned();
+pub fn runfromlib(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let paths = fs::read_dir(path)?;
+    for path in paths {
+        if let Some(path) = path.ok() {
+            println!("Name of the file {}", path.path().display());
+        }
+    }
     // https://stackoverflow.com/questions/68865499/include-str-set-string-literal-path
-    let markdown = include_str!(path);
-    let html = to_html(markdown);
-    println!("{}", html);
+    //  let markdown = include_str!(path);
+    //  let html = to_html(markdown);
+    //  println!("{}", html);
+    Ok(())
 }
