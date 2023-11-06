70535506228757e7c9f006dfb56a77e8698e4d67
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2023-11-06 04:33:10 -0600

    wip

--- rust-web-development/markdown-html/src/lib/mod.rs
+++ rust-web-development/markdown-html/src/lib/mod.rs
@@ -5,7 +5,10 @@ pub fn runfromlib(path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let paths = fs::read_dir(path)?;
     for path in paths {
         if let Some(path) = path.ok() {
-            println!("Name of the file {}", path.path().display());
+            let path = path.path().into_os_string();
+            let content = fs::read_to_string(path)?;
+            println!("{}", content);
+            //println!("Name of the file {}", path.path().display());
         }
     }
     // https://stackoverflow.com/questions/68865499/include-str-set-string-literal-path
