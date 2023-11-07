24467d16dd0c9f3061796f605277488a0e98a939
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2023-11-07 04:22:24 -0600

    wip

--- rust-web-development/markdown-html/src/lib/mod.rs
+++ rust-web-development/markdown-html/src/lib/mod.rs
@@ -1,19 +1,21 @@
 use std::fs;
 
 use markdown::to_html;
+// read from a directory
+// convert to markdown
+// use wehooks for triggering generate
+// make an api request to github directory
+// read the contents
+// convert to markdown
 pub fn runfromlib(path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let paths = fs::read_dir(path)?;
     for path in paths {
         if let Some(path) = path.ok() {
             let path = path.path().into_os_string();
             let content = fs::read_to_string(path)?;
-            println!("{}", content);
-            //println!("Name of the file {}", path.path().display());
+            let markdown = to_html(&content);
+            println!("{}", markdown);
         }
     }
-    // https://stackoverflow.com/questions/68865499/include-str-set-string-literal-path
-    //  let markdown = include_str!(path);
-    //  let html = to_html(markdown);
-    //  println!("{}", html);
     Ok(())
 }
