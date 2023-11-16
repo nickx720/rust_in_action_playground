f263eb50b25c199a41649140626f36313796045d
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2023-11-16 03:27:14 -0500

    wip

--- rust-web-development/markdown-html/src/lib/server/mod.rs
+++ rust-web-development/markdown-html/src/lib/server/mod.rs
@@ -1,12 +1,37 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::future::{ready, Ready};
+
+use actix_web::{
+    dev::{Service, ServiceRequest, ServiceResponse, Transform},
+    get, post, web, App, Error, HttpResponse, HttpServer, Responder,
+};
 use serde::Deserialize;
 
 // http://danielwelch.github.io/rust-web-service.html
+// https://actix.rs/docs/middleware
 
 #[derive(Deserialize)]
 struct PushEvent {
     #[serde(rename = "ref")]
     reference: String,
+}
+
+struct VerifySignature;
+
+impl<S, B> Transform<S, ServiceRequest> for VerifySignature
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = VerifySignature<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(VerifySignature { service }))
+    }
 }
 
 #[get("/webhook")]
