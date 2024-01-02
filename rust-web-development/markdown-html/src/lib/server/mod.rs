4c1caa08c7f8d0fe4ec9aaa7dde31f944c4bc711
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2024-01-02 03:46:32 -0500

    works, but need to try with docs

--- rust-web-development/markdown-html/src/lib/server/mod.rs
+++ rust-web-development/markdown-html/src/lib/server/mod.rs
@@ -140,7 +140,7 @@ async fn read_contents_repo() -> Result<impl Responder, Box<dyn std::error::Erro
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0",
             ),
         );
-        let url = format!("{}/contents/docs", url.repo);
+        let url = format!("{}/contents/README.MD", url.repo);
         let client = reqwest::Client::builder()
             .default_headers(headers)
             .build()?;
