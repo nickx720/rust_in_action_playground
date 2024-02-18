d1d5374cff9710c08065a2dfc70059a8ab1a3b93
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-02-18 03:26:24 -0500

    Replacing logger crate with log4rs

--- rust-web-development/chapter3/src/main.rs
+++ rust-web-development/chapter3/src/main.rs
@@ -8,7 +8,7 @@ mod types;
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
     log::error!("This is an error!");
     log::info!("This is info");
     log::warn!("This is warning");
