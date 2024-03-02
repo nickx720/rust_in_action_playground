d7352951c4015dcf6b990579b5cb554ba3735897
Nick <34407800+nickx720@users.noreply.github.com>
2024-03-02 07:48:47 -0500

    wip (#167)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/chapter3/src/main.rs
+++ rust-web-development/chapter3/src/main.rs
@@ -2,12 +2,16 @@
 
 use handle_errors::return_error;
 use warp::{http::Method, Filter};
+
+use tracing_subscriber::fmt::format::FmtSpan;
 mod routes;
 mod store;
 mod types;
 
 #[tokio::main]
 async fn main() {
+    let log_filter = std::env::var(";RUST_LOG")
+        .unwrap_or_else(|_| "practical_rust_book=info,warp=error".to_owned());
     let store = store::Store::new();
     let store_filter = warp::any().map(move || store.clone());
 
@@ -15,14 +19,15 @@ async fn main() {
         .allow_any_origin()
         .allow_header("content-type")
         .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
-
-    let id_filter = warp::any().map(|| uuid::Uuid::new_v4().to_string());
+    tracing_subscriber::fmt()
+        .with_env_filter(log_filter)
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
     let get_questions = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and(warp::query())
         .and(store_filter.clone())
-        .and(id_filter)
         .and_then(routes::question::get_questions);
 
     let update_question = warp::put()
