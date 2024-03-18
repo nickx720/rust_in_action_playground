a098bd8f12840a49c4c2e7007228c55f5af31bf9
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-03-18 04:52:33 -0400

    Adding new macros

--- rust-web-development/learn-macros/src/main.rs
+++ rust-web-development/learn-macros/src/main.rs
@@ -1,11 +1,25 @@
-#![feature(trace_macros)]
-#![feature(log_syntax)]
-use crate::greeting::base_greeting_fn;
+//use crate::greeting::base_greeting_fn;
+
+use crate::calculate::{FirstName, LastName};
+//#[macro_use]
+//mod greeting;
 #[macro_use]
-mod greeting;
+mod calculate;
+struct Age {
+    value: String,
+}
 fn main() {
-    trace_macros!(true);
-    greeting!("Nick", "Heya");
-    greeting!(test "Heya");
-    trace_macros!(false);
+    generate_get_value!(FirstName, String);
+    generate_get_value!(Age, String);
+    let first = FirstName::new("John");
+    let value_of = first.unwrap().get_value().clone();
+    let age = Age::new("Cortanna");
+    let age_va = age.unwrap().get_value().clone();
+
+    dbg!(value_of);
+    dbg!(age_va);
+    //    trace_macros!(true);
+    //    greeting!("Nick", "Heya");
+    //    greeting!(test "Heya");
+    //    trace_macros!(false);
 }
