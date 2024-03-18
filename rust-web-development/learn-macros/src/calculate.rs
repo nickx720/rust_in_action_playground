37ae283da457289a24fecdac30d2578902e65919
Nick <34407800+nickx720@users.noreply.github.com>
2024-03-18 04:52:57 -0400

    Adding new macros (#183)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

---
+++ rust-web-development/learn-macros/src/calculate.rs
@@ -0,0 +1,41 @@
+pub struct FirstName {
+    pub value: String,
+}
+
+//impl FirstName {
+//    pub fn new(name: &str) -> Result<FirstName, String> {
+//        if name.len() < 2 {
+//            Err("Name should be atleast two characters".to_string())
+//        } else {
+//            Ok(FirstName {
+//                value: name.to_string(),
+//            })
+//        }
+//    }
+//    pub fn get_value(&self) -> &String {
+//        &self.value
+//    }
+//}
+
+pub struct LastName {
+    pub value: String,
+}
+
+macro_rules! generate_get_value {
+    ($struct_type:ident, $return_type: ty) => {
+        impl $struct_type {
+            pub fn new(value: &str) -> Result<$struct_type, String> {
+                if value.len() < 2 {
+                    Err("Name should be atleast two characters".to_string())
+                } else {
+                    Ok($struct_type {
+                        value: value.to_string(),
+                    })
+                }
+            }
+            pub fn get_value(&self) -> &$return_type {
+                &self.value
+            }
+        }
+    };
+}
