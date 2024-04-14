47821108bf7c3ebbed5ef32a505fbdd3e66fcee2
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-04-14 05:23:55 -0400

    postgres setup completed

--- rust-web-development/chapter3/src/store.rs
+++ rust-web-development/chapter3/src/store.rs
@@ -5,7 +5,7 @@ use sqlx::{
 };
 
 use crate::types::{
-    answer::{Answer, NewAnswer},
+    answer::{Answer, AnswerId, NewAnswer},
     question::{NewQuestion, Question, QuestionId},
 };
 
@@ -92,6 +92,22 @@ impl Store {
         }
     }
     pub async fn add_answer(&self, new_answer: NewAnswer) -> Result<Answer, Error> {
-        todo!()
+        match sqlx::query("INSERT INTO answers (content,question_id) VALUES ($1, $2)")
+            .bind(new_answer.content)
+            .bind(new_answer.question_id.0)
+            .map(|row: PgRow| Answer {
+                id: AnswerId(row.get("id")),
+                content: row.get("content"),
+                question_id: QuestionId(row.get("question_id")),
+            })
+            .fetch_one(&self.connection)
+            .await
+        {
+            Ok(answer) => Ok(answer),
+            Err(e) => {
+                tracing::event!(tracing::Level::ERROR, "{:?}", e);
+                Err(Error::DatabaseQueryError(e))
+            }
+        }
     }
 }
