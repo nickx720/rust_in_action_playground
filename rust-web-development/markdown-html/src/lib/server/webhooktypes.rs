52cddfdaa8df9253efc1c0e6889246229f8687bb
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-05-11 02:04:08 -0400

    removing owner duplicates

--- rust-web-development/markdown-html/src/lib/server/webhooktypes.rs
+++ rust-web-development/markdown-html/src/lib/server/webhooktypes.rs
@@ -318,7 +318,7 @@ pub struct RepositoryPush {
     #[serde(rename = "full_name")]
     pub full_name: String,
     pub private: bool,
-    pub owner: Owner,
+    pub owner: Owner1,
     #[serde(rename = "html_url")]
     pub html_url: String,
     pub description: Value,
@@ -459,7 +459,7 @@ pub struct RepositoryPush {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Owner {
+pub struct Owner1 {
     pub name: String,
     pub email: String,
     pub login: String,
@@ -947,44 +947,6 @@ pub struct Repo {
     pub merge_commit_message: String,
     #[serde(rename = "merge_commit_title")]
     pub merge_commit_title: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Owner {
-    pub login: String,
-    pub id: i64,
-    #[serde(rename = "node_id")]
-    pub node_id: String,
-    #[serde(rename = "avatar_url")]
-    pub avatar_url: String,
-    #[serde(rename = "gravatar_id")]
-    pub gravatar_id: String,
-    pub url: String,
-    #[serde(rename = "html_url")]
-    pub html_url: String,
-    #[serde(rename = "followers_url")]
-    pub followers_url: String,
-    #[serde(rename = "following_url")]
-    pub following_url: String,
-    #[serde(rename = "gists_url")]
-    pub gists_url: String,
-    #[serde(rename = "starred_url")]
-    pub starred_url: String,
-    #[serde(rename = "subscriptions_url")]
-    pub subscriptions_url: String,
-    #[serde(rename = "organizations_url")]
-    pub organizations_url: String,
-    #[serde(rename = "repos_url")]
-    pub repos_url: String,
-    #[serde(rename = "events_url")]
-    pub events_url: String,
-    #[serde(rename = "received_events_url")]
-    pub received_events_url: String,
-    #[serde(rename = "type")]
-    pub type_field: String,
-    #[serde(rename = "site_admin")]
-    pub site_admin: bool,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
