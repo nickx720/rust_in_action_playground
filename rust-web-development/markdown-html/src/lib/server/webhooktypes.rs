a634d3504d9af75f0f324f9868b47f9f24046cd6
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-05-12 02:29:20 -0400

    Removing duplicate sender field

--- rust-web-development/markdown-html/src/lib/server/webhooktypes.rs
+++ rust-web-development/markdown-html/src/lib/server/webhooktypes.rs
@@ -506,44 +506,6 @@ pub struct Pusher {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Sender {
-    pub login: String,
-    pub id: i64,
-    #[serde(rename = "node_id")]
-    pub node_id: String,
-    #[serde(rename = "avatar_url")]
-    pub avatar_url: String,
-    #[serde(rename = "gravatar_id")]
-    pub gravatar_id: String,
-    pub url: String,
-    #[serde(rename = "html_url")]
-    pub html_url: String,
-    #[serde(rename = "followers_url")]
-    pub followers_url: String,
-    #[serde(rename = "following_url")]
-    pub following_url: String,
-    #[serde(rename = "gists_url")]
-    pub gists_url: String,
-    #[serde(rename = "starred_url")]
-    pub starred_url: String,
-    #[serde(rename = "subscriptions_url")]
-    pub subscriptions_url: String,
-    #[serde(rename = "organizations_url")]
-    pub organizations_url: String,
-    #[serde(rename = "repos_url")]
-    pub repos_url: String,
-    #[serde(rename = "events_url")]
-    pub events_url: String,
-    #[serde(rename = "received_events_url")]
-    pub received_events_url: String,
-    #[serde(rename = "type")]
-    pub type_field: String,
-    #[serde(rename = "site_admin")]
-    pub site_admin: bool,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Commit {
     pub id: String,
     #[serde(rename = "tree_id")]
