a3f0c7aaf3e40eda8261490c3a24da02ca376512
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-05-13 04:07:28 -0400

    Sample push

--- rust-web-development/markdown-html/src/lib/server/mod.rs
+++ rust-web-development/markdown-html/src/lib/server/mod.rs
@@ -74,7 +74,7 @@ async fn webhook() -> Result<impl Responder, WebHookError> {
         let webhook_input = sample
             .active(true)
             .events(vec!["push".to_string(), "pull_request".to_string()])
-            .url("https://4d2573bc4eec36.lhr.life/engaged".to_string())
+            .url("https://cad15c0d13babc.lhr.life/engaged".to_string())
             .content_type("json".to_string())
             .insecure_ssl(0.to_string())
             .builder()
