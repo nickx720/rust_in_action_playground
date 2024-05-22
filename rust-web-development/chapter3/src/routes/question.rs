1750910aa17dcc85340cf72061636e24586ebf97
Nick <34407800+nickx720@users.noreply.github.com>
2024-05-22 09:21:37 -0400

    wip (#247)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/chapter3/src/routes/question.rs
+++ rust-web-development/chapter3/src/routes/question.rs
@@ -35,6 +35,20 @@ pub async fn update_question(
     store: Store,
     question: Question,
 ) -> Result<impl warp::Reply, warp::Rejection> {
+    let title = match check_profanity(question.title).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
+    let content = match check_profanity(question.content).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
+    let question = Question {
+        id: question.id,
+        title,
+        content,
+        tags: question.tags,
+    };
     let res = match store.update_question(question, id).await {
         Ok(res) => res,
         Err(e) => return Err(warp::reject::custom(e)),
