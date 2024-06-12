67d65ec79fbe75d97b60b0e4371851c520b10d7d
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-06-12 02:34:26 -0400

    ADding chrono to expire issue token

--- rust-web-development/chapter3/src/routes/authentication.rs
+++ rust-web-development/chapter3/src/routes/authentication.rs
@@ -1,7 +1,7 @@
 use crate::types::account::Account;
 use crate::{store::Store, types::account::AccountId};
 use argon2::{self, Config};
-use paseto::v1::local::local_paseto;
+use chrono::prelude::*;
 use rand::Rng;
 use warp::http::StatusCode;
 
@@ -30,9 +30,16 @@ fn verify_password(hash: &str, password: &[u8]) -> Result<bool, argon2::Error> {
 }
 
 fn issue_token(account_id: AccountId) -> String {
-    let state = serde_json::to_string(&account_id).expect("Failed to serialize state");
-    local_paseto(&state, None, "RANDOM WORDS WINTER MACINTOSH PC".as_bytes())
-        .expect("Failed to create token")
+    let current_date_time = Utc::now();
+    let dt = current_date_time + chrono::Duration::days(1);
+
+    paseto::PasetoBuilder::new()
+        .set_encryption_key(&Vec::from("RANDOM WORDS WINTER MACINTOSH PC".as_bytes()))
+        .set_expiration(&dt)
+        .set_not_before(&Utc::now())
+        .set_claim("account_id", serde_json::json!(account_id))
+        .build()
+        .expect("Failed to construct paseto token w/ builder!")
 }
 
 pub async fn register(store: Store, account: Account) -> Result<impl warp::Reply, warp::Rejection> {
