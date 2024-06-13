707fd11b0c8887f0299bba3e1c2e14aaf184c781
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-06-13 06:04:15 -0400

    Adding session

---
+++ rust-web-development/chapter3/migrations/20240613095704_extend_questions_table.down.sql
@@ -0,0 +1,2 @@
+-- Add down migration script here
+ALTER TABLE questions DROP COLUMN account_id serial;
---
+++ rust-web-development/chapter3/migrations/20240613095704_extend_questions_table.up.sql
@@ -0,0 +1,2 @@
+-- Add up migration script here
+ALTER TABLE questions ADD COLUMN account_id serial;
