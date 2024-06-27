8dab43972eafe0115fdc6a0dc58dc3d728e424d9
Nick <34407800+nickx720@users.noreply.github.com>
2024-06-27 02:37:18 -0400

    Adding enum for SocketType (#284)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/types/src/lib.rs
+++ rust-web-development/beej-socket/types/src/lib.rs
@@ -17,22 +17,36 @@ impl Display for Family {
     }
 }
 
-impl Into<libc::c_int> for Family {
-    fn into(self) -> libc::c_int {
-        match self {
-            Family::Ipv4 => libc::AF_INET,
-            Family::Ipv6 => libc::AF_INET6,
-            Family::Unspecified => libc::AF_UNSPEC,
-        }
-    }
-}
-
 impl From<libc::c_int> for Family {
     fn from(value: libc::c_int) -> Self {
         match value {
             libc::AF_INET => Family::Ipv4,
             libc::AF_INET6 => Family::Ipv6,
             _ => Family::Unspecified,
+        }
+    }
+}
+//impl Into<libc::c_int> for Family {
+//    fn into(self) -> libc::c_int {
+//        match self {
+//            Family::Ipv4 => libc::AF_INET,
+//            Family::Ipv6 => libc::AF_INET6,
+//            Family::Unspecified => libc::AF_UNSPEC,
+//        }
+//    }
+//}
+#[derive(Debug, Clone, PartialEq)]
+pub enum SocketType {
+    Stream,
+    Datagram,
+}
+
+impl From<libc::c_int> for SocketType {
+    fn from(value: libc::c_int) -> Self {
+        match value {
+            libc::SOCK_STREAM => SocketType::Stream,
+            libc::SOCK_DGRAM => SocketType::Datagram,
+            _ => panic!("Unknown socket type"),
         }
     }
 }
