76ef68cdfdf3e3e387c74340cd43700aeacb9994
Nick <34407800+nickx720@users.noreply.github.com>
2024-06-29 03:40:11 -0400

    wip (#286)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/builders/src/lib.rs
+++ rust-web-development/beej-socket/builders/src/lib.rs
@@ -1,5 +1,3 @@
-use std::mem;
-
 use typed_builder::TypedBuilder;
 use types::{Family, Flag, SocketType};
 
@@ -13,14 +11,12 @@ pub struct AddrInfo {
     flags: Flag,
 }
 
-impl Into<libc::addrinfo> for AddrInfo {
-    fn into(self) -> libc::addrinfo {
-        unsafe {
-            let mut addrinfo: libc::addrinfo = mem::zeroed();
-            addrinfo.ai_family = self.family.into();
-            addrinfo.ai_socktype = self.socktype.into();
-            addrinfo.ai_flags = self.flags.into();
-            addrinfo
+impl From<libc::addrinfo> for AddrInfo {
+    fn from(value: libc::addrinfo) -> Self {
+        Self {
+            family: value.ai_family.into(),
+            socktype: value.ai_socktype.into(),
+            flags: value.ai_flags.into(),
         }
     }
 }
