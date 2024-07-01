2dd1a946abb90d92343ab3843a6e998fa496a770
Nick <34407800+nickx720@users.noreply.github.com>
2024-07-01 02:18:51 -0400

    wip (#288)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/beej-examples/src/showip/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/showip/mod.rs
@@ -1,5 +1,21 @@
+use builders::AddrInfo;
+use nix::libc;
+use std::{ffi::CString, ptr};
 
+use types::Family;
 
-pub fn show_ip(host: String, ,service: String) -> i32 {
+pub fn show_ip(host: String, family: Family, service: String) -> i32 {
+    println!("IP addresses for {}\n", host);
+    let host = CString::new(host).expect("Invalid host");
+    let c_host: *const libc::c_char = host.as_ptr() as *const libc::c_char;
+
+    let service =
+        CString::new(service).expect("Invalid service, service should map to port number");
+    let service: *const libc::c_char = service.as_ptr() as *const libc::c_char;
+    let addrinfo = AddrInfo::builder().family(family).build();
+    let hints: AddrInfo = addrinfo.into();
+    let mut res = ptr::null_mut();
+    // TODO fix the type mismatch
+    unsafe { libc::getaddrinfo(c_host, service, &hints, &mut res) };
     todo!()
 }
