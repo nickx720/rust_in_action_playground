8769da94244456e08dae9cfa3b3ae705e6d3fc05
Nick <34407800+nickx720@users.noreply.github.com>
2024-07-14 03:59:54 -0400

    wip (#301)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/src/main.rs
+++ rust-web-development/beej-socket/src/main.rs
@@ -1,5 +1,6 @@
 use beej_examples::{self, showip::show_ip};
 use clap::{Parser, Subcommand};
+use types::Family;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -9,13 +10,20 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
-    ShowIp { host: String, service: String },
+    ShowIp {
+        host: String,
+        family: Family,
+        service: String,
+    },
 }
 
 fn main() {
     let args = Args::parse();
     let output = match args.command {
-        Commands::ShowIp { host, service } => show_ip(host, service),
+        Commands::ShowIp {
+            host,
+            family,
+            service,
+        } => show_ip(host, family, service).expect("Something went wrong displaying ip"),
     };
-    println!("The result of compute is {}", output);
 }
