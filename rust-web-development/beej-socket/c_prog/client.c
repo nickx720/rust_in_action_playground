64b9d54240727d17a3be86362855fa4159896942
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-07-25 02:22:15 -0400

    Adding ex and client.c

---
+++ rust-web-development/beej-socket/c_prog/client.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arp/inet.h>
+
+#define PORT 3490
+#define MAXDATASIZE 100
+
+void *get_in_addr(struct sockaddr *sa){
+  if(sa->sa_family == AF_INET){
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
