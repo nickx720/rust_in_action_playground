34784c5f077a97d4a047ad3f1654714eba826be1
Nick <34407800+nickx720@users.noreply.github.com>
2024-08-01 02:27:09 -0400

    Work in progress (#319)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/beej-examples/src/streamserver/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/streamserver/mod.rs
@@ -3,9 +3,37 @@ use std::{
     mem, ptr,
 };
 
+use builders::AddrInfo;
 use socket2::SockAddr;
+use types::Family;
 
 /// Simple Stream Server
 pub fn streamserver() {
+    let family = Family::Unspecified;
+    let host = "localhost";
+    let service = "3490";
+
+    let addrinfo = AddrInfo::builder()
+        .family(family)
+        .flags(types::Flag::Passive)
+        .build();
+    let hints: libc::addrinfo = addrinfo.into();
+
+    let mut servinfo = ptr::null_mut();
+    let rv = unsafe {
+        let node = CString::new(host).expect("Invalid node");
+        let c_node: *const libc::c_char = node.as_ptr() as *const libc::c_char;
+        let port = CString::new(service).expect("Invalid port");
+        let c_port: *const libc::c_char = port.as_ptr() as *const libc::c_char;
+
+        println!("Starting server in {host}:{service}");
+        libc::getaddrinfo(c_node, c_port, &hints, &mut servinfo)
+    };
+    if rv != 0 {
+        eprintln!("getaddrinfo: {}", unsafe {
+            CStr::from_ptr(libc::gai_strerror(rv)).to_str().unwrap()
+        });
+        return;
+    }
     todo!()
 }
