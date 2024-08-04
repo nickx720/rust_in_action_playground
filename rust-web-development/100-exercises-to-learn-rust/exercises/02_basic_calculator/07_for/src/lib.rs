4ca34174b7d8829027f0b3278af1f70482d772f9
Nick <34407800+nickx720@users.noreply.github.com>
2024-08-04 10:39:04 -0400

    for loop (#322)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/100-exercises-to-learn-rust/exercises/02_basic_calculator/07_for/src/lib.rs
+++ rust-web-development/100-exercises-to-learn-rust/exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,10 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut factorial = 1;
+    for number in 1..=n {
+        factorial *= number
+    }
+    factorial
 }
 
 #[cfg(test)]
