9c88cdcfc8551edeb3285cf11f5e44282929baea
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-08-09 03:19:09 -0400

    client wip

--- rust-web-development/beej-socket/beej-examples/src/streamclient/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/streamclient/mod.rs
@@ -1,4 +1,4 @@
-use std::{ffi::CString, net::IpAddr, ptr};
+use std::{ffi::CString, mem, net::IpAddr, ptr};
 
 use builders::AddrInfo;
 
@@ -42,6 +42,33 @@ pub fn streamclient(host: String) {
         let node = CString::new(host).expect("Invalid host");
         let c_node: *const libc::c_char = node.as_ptr() as *const libc::c_char;
         let service = CString::new(service).expect("Invalid service");
-        todo!()
+        let c_service: *const libc::c_char = service.as_ptr() as *const libc::c_char;
+        let hints = hints.into();
+        libc::getaddrinfo(c_node, c_service, &hints, &mut servinfo);
     }
+    let mut sockfd = mem::MaybeUninit::<libc::c_int>::uninit();
+    while !servinfo.is_null() {
+        unsafe {
+            let _sockfd = libc::socket(
+                (*servinfo).ai_family,
+                (*servinfo).ai_socktype,
+                (*servinfo).ai_protocol,
+            );
+            if _sockfd == -1 {
+                servinfo = (*servinfo).ai_next as *mut libc::addrinfo;
+                continue;
+            }
+            let errr = libc::connect(_sockfd, (*servinfo).ai_addr, (*servinfo).ai_addrlen);
+            if errr == -1 {
+                libc::close(_sockfd);
+                servinfo = (*servinfo).ai_next as *mut libc::addrinfo;
+                eprintln!("client: connect err");
+                continue;
+            }
+            sockfd.write(_sockfd)
+        }
+        break;
+    }
+    // if servinfo.is_null()
+    todo!()
 }
