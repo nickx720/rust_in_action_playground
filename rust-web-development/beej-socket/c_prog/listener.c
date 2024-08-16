c896deee7d98334a34c5b65e34da057b986afab3
Nick <34407800+nickx720@users.noreply.github.com>
2024-08-16 07:30:41 -0400

    completing listener.c without compiling (#334)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/c_prog/listener.c
+++ rust-web-development/beej-socket/c_prog/listener.c
@@ -35,6 +35,40 @@ int main(void){
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_DGRAM;
   hints.ai_flags = AI_PASSIVE;
-}
 
+  if((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0){
+    fprintf(stderr, "getaddrinfo: %s\n",gai_strerror(rv));
+    return 1;
+  }
+
+  for (p=servinfo; p!= NULL; p=p->ai_next){
+    if ((sockfd = socket(p->ai_family,p->ai_socktype, p->ai_protocol)) == -1){
+      perror("listener: socket");
+      continue;
+    }
+    if(bind(sockfd, p-> ai_addr, p->ai_addrlen) == -1){
+      close(sockfd);
+      perror("listener: bind");
+      continue;
+    }
+    break;
+  }
+  if (p == NULL){
+    fprintf(stderr,"listener: failed to bind socket\n");
+    return 2;
+  }
+  freeaddrinfo(servinfo);
+  printf("Listener: waiting to recvfrom...\n");
+  addr_len = sizeof(their_addr);
+  if ((numbytes = recvfrom(sockfd,buf,MAXBUFLEN-1,0,(struct sockaddr *)&their_addr,&addr_len)) == -1){
+    perror("recvfrom");
+    exit(1);
+  }
+  printf("listener: got packet from %s\n", inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr),s, sizeof s));
+  printf("listener: packet is %d bytes long\n",numbytes);
+  buf[numbytes] = '\0';
+  printf("listener: packet contains \"%s\"\n",buf);
+  close(sockfd);
+  return 0;
+}
 
