c1f727d1ec9bcd36cc13c4eb607f80be5b3f252e
Nick <34407800+nickx720@users.noreply.github.com>
2024-08-21 02:25:40 -0400

    Handling SocketAddr::V4 (#339)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/beej-examples/src/listener/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/listener/mod.rs
@@ -1,4 +1,4 @@
-use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
+use std::{net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6}, os::fd::AsRawFd};
 
 use types::Family;
 
@@ -9,4 +9,17 @@ pub fn socketlistener(port: u16, family: Family) {
             SocketAddr::V6(SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, port, 0, 0))
         }
     };
+    let sockfd = match local_addr{
+        SocketAddr::V4(localhost) =>{
+            let socket: nix::sys::socket::SockaddrIn = localhost.into();
+            let sockfd = nix::sys::socket::socket(nix::sys::socket::AddressFamily::Inet, nix::sys::socket::SockType::Datagram, nix::sys::socket::SockFlag::empty(), None).expect("Failed to create socket");
+            nix::sys::socket::bind(sockfd.as_raw_fd(),&socket).expect("Failed to bind to socket");
+            let ss: nix::sys::socket::SockaddrStorage = nix::sys::socket::getsockname(sockfd.as_raw_fd()).expect("getsockname");
+            println!("Listening on {}",ss.as_sockaddr_in().expect("sockaddr not ipv4"));
+            sockfd
+        }
+        SocketAddr::V6(localhost) =>{
+todo!()
+        };
+    }
 }
