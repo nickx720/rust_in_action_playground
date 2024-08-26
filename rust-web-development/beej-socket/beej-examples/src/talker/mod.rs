accc1661f91eb4346ff4b3f01b2e1cc21ab3c9bc
Nick <34407800+nickx720@users.noreply.github.com>
2024-08-26 03:45:43 -0400

    wip talker (#344)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/beej-examples/src/talker/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/talker/mod.rs
@@ -1,5 +1,5 @@
 use std::{
-    net::{IpAddr, SocketAddrV4},
+    net::{IpAddr, SocketAddrV4, SocketAddrV6},
     os::fd::AsRawFd,
 };
 
@@ -23,6 +23,23 @@ pub fn sockettalker(host: IpAddr, port: u16, message: String) {
             )
             .expect("Failed to send message");
         }
-        IpAddr::V6(addr) => {}
+        IpAddr::V6(addr) => {
+            let socket = SocketAddrV6::new(addr, port, 0, 0);
+            let socket: nix::sys::socket::SockaddrIn6 = socket.into();
+            let sockfd = nix::sys::socket::socket(
+                nix::sys::socket::AddressFamily::Inet6,
+                nix::sys::socket::SockType::Datagram,
+                nix::sys::socket::SockFlag::empty(),
+                None,
+            )
+            .expect("Failed to create sockfd");
+            nix::sys::socket::sendto(
+                sockfd.as_raw_fd(),
+                message.as_bytes(),
+                &socket,
+                nix::sys::socket::MsgFlags::empty(),
+            )
+            .expect("Failed to send message");
+        }
     }
 }
