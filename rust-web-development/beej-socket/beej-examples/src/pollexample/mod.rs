7edfadadf23e3f40c89b309bbf8e98fb784aeed9
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-08-29 02:59:20 -0400

    wip

--- rust-web-development/beej-socket/beej-examples/src/pollexample/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/pollexample/mod.rs
@@ -1,1 +1,24 @@
-pub fn pollexample() {}
+use std::{
+    net::{IpAddr, SocketAddrV4},
+    os::fd::AsFd,
+};
+
+use nix::poll::{PollFd, PollFlags};
+
+pub fn pollexample(host: IpAddr, port: u16) {
+    match host {
+        IpAddr::V4(addr) => {
+            let socket = SocketAddrV4::new(addr, port);
+            let socket: nix::sys::socket::SockaddrIn = socket.into();
+            let sockfd = nix::sys::socket::socket(
+                nix::sys::socket::AddressFamily::Inet,
+                nix::sys::socket::SockType::Datagram,
+                nix::sys::socket::SockFlag::empty(),
+                None,
+            )
+            .expect("Failed to create sockfd");
+            let pfd = PollFd::new(sockfd.as_fd(), PollFlags::POLLIN);
+        }
+        _ => panic!("not implemented"),
+    }
+}
