ec236e18458017d0c1925f9ce309128bd90d2786
Nick <34407800+nickx720@users.noreply.github.com>
2024-09-06 02:53:48 -0400

    wip (#355)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/beej-examples/src/pollexample/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/pollexample/mod.rs
@@ -1,36 +1,25 @@
 use std::{
-    net::{IpAddr, SocketAddrV4},
-    os::fd::AsFd,
+    fs::File,
+    os::fd::{AsFd, FromRawFd},
 };
 
-use nix::{
-    poll::{PollFd, PollFlags, PollTimeout},
-    unistd::pipe,
-};
+use nix::poll::{PollFd, PollFlags, PollTimeout};
 
-pub fn pollexample(host: IpAddr) {
-    match host {
-        IpAddr::V4(addr) => {
-            let socket = SocketAddrV4::new(addr, 0);
-            let (read_fd, write_fd) = pipe().expect("Something went wrong");
-            println!("Hit return or wait 2.5 seconds");
-            // TODO return on enter, listen for keyboard
-            let pfd = PollFd::new(read_fd.as_fd(), PollFlags::POLLIN);
-            let mut list_pfd: [PollFd; 1] = [pfd];
-            let timeout = 2500u16;
-            let timeout: PollTimeout = timeout.into();
-            let num_events = nix::poll::poll(&mut list_pfd, timeout).expect("No num event created");
-            if num_events == 0 {
-                println!("Poll timeout");
-            } else {
-                let poll_event = pfd.revents().expect("Poll Flag not created");
-                if poll_event.contains(PollFlags::POLLIN) {
-                    println!("Yahoo");
-                }
-            }
-            //            nix::unistd::close(read_fd).unwrap();
-            //            nix::unistd::close(write_fd).unwrap();
+pub fn pollexample() {
+    println!("Hit return or wait 2.5 seconds");
+    // TODO return on enter, listen for keyboard
+    let file: File = unsafe { File::from_raw_fd(0) };
+    let pfd = PollFd::new(file.as_fd(), PollFlags::POLLIN);
+    let mut list_pfd: [PollFd; 1] = [pfd];
+    let timeout = 2500u16;
+    let timeout: PollTimeout = timeout.into();
+    let num_events = nix::poll::poll(&mut list_pfd, timeout).expect("No num event created");
+    if num_events == 0 {
+        println!("Poll timeout");
+    } else {
+        let poll_event = pfd.revents().expect("Poll Flag not created");
+        if poll_event.contains(PollFlags::POLLIN) {
+            println!("Yahoo");
         }
-        _ => panic!("not implemented"),
     }
 }
