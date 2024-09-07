1b9987058e7628c59e87b1285dab935d01ba31f1
Nick <34407800+nickx720@users.noreply.github.com>
2024-09-07 08:09:41 -0400

    Setters done (#356)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/100-exercises-to-learn-rust/exercises/03_ticket_v1/07_setters/src/lib.rs
+++ rust-web-development/100-exercises-to-learn-rust/exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -10,22 +10,27 @@ pub struct Ticket {
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
+    fn is_empty(string: &String, label: &str) {
+        if string.is_empty() {
+            panic!("{} cannot be empty", label)
         }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
+    }
+    fn len(string: &String, len: usize, label: &str) {
+        if string.len() > len {
+            panic!("{} cannot be longer than {} bytes", label, len)
         }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
+    }
+    fn status_check(status: &String) {
         if status != "To-Do" && status != "In Progress" && status != "Done" {
             panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
         }
+    }
+    pub fn new(title: String, description: String, status: String) -> Ticket {
+        Ticket::is_empty(&title, "Title");
+        Ticket::len(&title, 50, "Title");
+        Ticket::is_empty(&description, "Description");
+        Ticket::len(&description, 500, "Description");
+        Ticket::status_check(&status);
 
         Ticket {
             title,
@@ -44,6 +49,20 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+    pub fn set_title(&mut self, title: String) {
+        Ticket::is_empty(&title, "Title");
+        Ticket::len(&title, 50, "Title");
+        self.title = title;
+    }
+    pub fn set_description(&mut self, description: String) {
+        Ticket::is_empty(&description, "Description");
+        Ticket::len(&description, 500, "Description");
+        self.description = description;
+    }
+    pub fn set_status(&mut self, status: String) {
+        Ticket::status_check(&status);
+        self.status = status;
     }
 }
 
