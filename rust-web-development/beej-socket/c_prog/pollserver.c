f670d56eddcc22f03920297cbdef0c8357a6d5c0
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-09-11 03:31:29 -0400

    wip

--- rust-web-development/beej-socket/c_prog/pollserver.c
+++ rust-web-development/beej-socket/c_prog/pollserver.c
@@ -24,5 +24,34 @@ int get_listener_socket(void) {
   int yes = 1;
   int rv;
   struct addrinfo hints, *ai, *p;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
+    fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+    exit(1);
+  }
+
+  for (p = ai; p != NULL; p = p->ai_next) {
+    listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    if (listener < 0) {
+      continue;
+    }
+
+    // Lose the pesky "address already in use" error message
+    setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+    if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
+      close(listener);
+      continue;
+    }
+    break;
+  }
+
+  // If we got here, it means we didn't getbound
+  if (p == NULL) {
+    return -1;
+  }
 }
 
