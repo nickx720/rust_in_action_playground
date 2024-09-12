fa5e1252437745df299cf8f376335e4a3ef2ebb7
Nick <34407800+nickx720@users.noreply.github.com>
2024-09-12 02:51:21 -0400

    wip (#361)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/c_prog/pollserver.c
+++ rust-web-development/beej-socket/c_prog/pollserver.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <cstdio>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <poll.h>
@@ -53,5 +54,32 @@ int get_listener_socket(void) {
   if (p == NULL) {
     return -1;
   }
+
+  freeaddrinfo(ai);
+
+  if (listen(listener, 10) == -1) {
+    return -1;
+  }
+  return listener;
+}
+// Add a new file descriptor to set
+void add_to_pfds(struct pollfd *pfds[], int newfd, int *fd_count,
+                 int *fd_size) {
+  // If we don't have room, add more space in the pfds array
+  if (*fd_count == *fd_size) {
+    *fd_size *= 2; // Double it
+    *pfds = realloc(*pfds, sizeof(**pfds) * (*fd_size));
+  }
+  (*pfds)[*fd_count].fd = newfd;
+  (*pfds)[*fd_count].events = POLLIN;
+
+  (*fd_count)++;
+}
+
+// Remove an index from set
+void del_from_pfds(struct pollfd pfds[], int i, int *fd_count) {
+  // Copy the one from the end over this one
+  pfds[i] = pfds[*fd_count - 1];
+  (*fd_count)--;
 }
 
