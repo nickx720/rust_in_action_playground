11d4e9c4f32d831bed6698f6ddfa0473c9cfece9
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-10-01 07:37:29 -0400

    wip

--- rust-web-development/beej-socket/beej-examples/src/pollserver/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/pollserver/mod.rs
@@ -1,4 +1,7 @@
-use std::net::{Ipv6Addr, SocketAddrV6};
+use std::{
+    net::{Ipv6Addr, SocketAddrV6},
+    os::fd::{AsFd, AsRawFd},
+};
 
 pub fn pollserver(port: u16) {
     let unspec = SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, port, 0, 0);
@@ -10,5 +13,14 @@ pub fn pollserver(port: u16) {
         None,
     )
     .expect("Failed to create socket");
+
+    nix::sys::socket::setsockopt(&listener, nix::sys::socket::sockopt::ReuseAddr, &true)
+        .expect("Failed to set socket options");
+
+    nix::sys::socket::bind(listener.as_raw_fd(), &socket).expect("Failed to bind to socket");
+    let backlog = nix::sys::socket::Backlog::new(10).expect("Failed to create backlog");
+    nix::sys::socket::listen(&listener, backlog).expect("Failed to listen on socket");
+
+    let listener_pfd = nix::poll::PollFd::new(listener.as_fd(), nix::poll::PollFlags::POLLIN);
     todo!()
 }
