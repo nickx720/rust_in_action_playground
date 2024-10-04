12a605a039ccd60c9a339f90d4ec1bf6c27245bc
Nick <34407800+nickx720@users.noreply.github.com>
2024-10-04 03:00:43 -0400

    Completed poll server (#381)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/beej-examples/src/pollserver/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/pollserver/mod.rs
@@ -47,6 +47,44 @@ pub fn pollserver(port: u16) {
                         let new_pfd = nix::poll::PollFd::new(new_bfd, nix::poll::PollFlags::POLLIN);
                         pfds.push(new_pfd);
                         // convert fd to address
+                        let ss: nix::sys::socket::SockaddrStorage =
+                            nix::sys::socket::getpeername(new_fd).expect("getpeername failed");
+                        println!("New connection, {ss:?}");
+                    } else {
+                        println!("[Client] preparing to read...");
+                        let mut buf = [0u8; 1024];
+                        let nbytes = nix::sys::socket::recv(
+                            pfd.as_fd().as_raw_fd(),
+                            &mut buf,
+                            nix::sys::socket::MsgFlags::empty(),
+                        )
+                        .expect("recv failed");
+                        println!("[Client] Received {} bytes", nbytes);
+                        // TODO nbytes
+                        if nbytes == 0 {
+                            println!("[Client] Connection closed");
+                            pfds.remove(i);
+                        } else {
+                            println!("[Client] Reading bytes...");
+                            // Send data to all clients but the sender and listener
+                            for j in 1..pfds.len() {
+                                if i != j {
+                                    let target_pfd = &pfds[j];
+                                    let ss: nix::sys::socket::SockaddrStorage =
+                                        nix::sys::socket::getpeername(
+                                            target_pfd.as_fd().as_raw_fd(),
+                                        )
+                                        .expect("getpeername failed");
+                                    nix::sys::socket::sendto(
+                                        target_pfd.as_fd().as_raw_fd(),
+                                        &buf[..nbytes],
+                                        &ss,
+                                        nix::sys::socket::MsgFlags::empty(),
+                                    )
+                                    .expect("sendto failed");
+                                }
+                            }
+                        }
                     }
                 }
             }
