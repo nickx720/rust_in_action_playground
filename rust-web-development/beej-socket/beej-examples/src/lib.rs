1f78154a0e15d05fc2c66a0a4b8481cc085ba01c
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-10-09 02:43:35 -0400

    wip

--- rust-web-development/beej-socket/beej-examples/src/lib.rs
+++ rust-web-development/beej-socket/beej-examples/src/lib.rs
@@ -1,7 +1,8 @@
 pub mod listener;
 pub mod pollexample;
+pub mod pollserver;
+pub mod selectexample;
 pub mod showip;
 pub mod streamclient;
 pub mod streamserver;
 pub mod talker;
-pub mod pollserver;
