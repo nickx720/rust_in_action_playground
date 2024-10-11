12dbfdc2f4f898f1e842d40aaf04f3ba632b45c7
Nick <34407800+nickx720@users.noreply.github.com>
2024-10-11 02:43:23 -0400

    wip (#388)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/beej-examples/src/selectexample/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/selectexample/mod.rs
@@ -1,4 +1,10 @@
+use std::{io, os::fd::AsFd};
+
 pub fn selectexample(port: u16) {
     let tv = nix::sys::time::TimeVal::new(2, 500000);
+    let mut fd_set = nix::sys::select::FdSet::new();
+    fd_set.clear();
+    let stdin = io::stdin();
+    fd_set.insert(stdin.as_fd());
     todo!()
 }
