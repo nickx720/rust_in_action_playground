59480693ee116704a4288272e923d837c72648e4
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-10-16 02:13:56 -0400

    wip

--- rust-web-development/beej-socket/beej-examples/src/selectexample/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/selectexample/mod.rs
@@ -3,12 +3,16 @@ use std::{
     os::fd::{AsFd, AsRawFd},
 };
 
-pub fn selectexample(port: u16) {
+pub fn selectexample() {
     let mut tv = nix::sys::time::TimeVal::new(2, 500000);
     let mut fd_set = nix::sys::select::FdSet::new();
     fd_set.clear();
     let stdin = io::stdin();
     fd_set.insert(stdin.as_fd());
     let _ = nix::sys::select::select(stdin.as_raw_fd(), &mut fd_set, None, None, &mut tv);
-    todo!()
+    if fd_set.contains(stdin.as_fd()) {
+        println!("A key was pressed\n");
+    } else {
+        println!("Nothing happened")
+    }
 }
