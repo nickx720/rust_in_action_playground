ca6d6df134691034ef639b74b9f0675d6d5a5fa2
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-10-18 02:44:40 -0400

    started work on selectserver

---
+++ rust-web-development/beej-socket/c_prog/selectserver.c
@@ -0,0 +1,24 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT "9034"
+
+void *get_in_addr(struct sockaddr *sa) {
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in *)sa)->sin_addr);
+  }
+  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+int main(void) {
+  fd_set master;
+  fd_set read_fds;
+}
+
