2e85c056be34556ffdd3b4d00db98d5728875dcb
Nick <34407800+nickx720@users.noreply.github.com>
2024-10-24 02:21:19 -0400

    wip (#401)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/c_prog/selectserver.c
+++ rust-web-development/beej-socket/c_prog/selectserver.c
@@ -41,5 +41,26 @@ int main(void) {
   FD_ZERO(&read_fds);
 
   memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
+    printf(stderr, "selectserver: %s\n", gai_strerror(rv));
+    exit(1);
+  }
+  for (p = ai; p != NULL; p = p->ai_next) {
+    listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    if (listener < 0) {
+      continue;
+    }
+    setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+    if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
+      close(listener);
+      continue;
+    }
+    break;
+  }
+  if (p == NULL) {
+  }
 }
 
