e4cfe4ea0d087e22d1ee657daf5fc004040db266
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>
2024-11-05 02:48:05 -0500

    wip

--- rust-web-development/beej-socket/beej-examples/src/selectserver/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/selectserver/mod.rs
@@ -1,4 +1,15 @@
+use std::net::{Ipv6Addr, SocketAddrV6};
+
 pub fn selectserver(port: u16) {
-    dbg!(port);
-    todo!()
+    let unspec = SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, port, 0, 0);
+    let socket: nix::sys::socket::SockaddrIn6 = unspec.into();
+    let listener = nix::sys::socket::socket(
+        nix::sys::socket::AddressFamily::Inet6,
+        nix::sys::socket::SockType::Stream,
+        nix::sys::socket::SockFlag::empty(),
+        None,
+    )
+    .expect("Failed to create socket");
+    nix::sys::socket::setsockopt(&listener, nix::sys::socket::sockopt::ReuseAddr, &true)
+        .expect("Failed to set socket options");
 }
