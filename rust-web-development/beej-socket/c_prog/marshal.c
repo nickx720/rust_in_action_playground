34898decee8ba8e7ee0bb7af4a98ae27d59b2253
Nick <34407800+nickx720@users.noreply.github.com>
2024-11-21 03:15:17 -0500

    wip (#429)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/c_prog/marshal.c
+++ rust-web-development/beej-socket/c_prog/marshal.c
@@ -26,3 +26,15 @@ float ntohf(uint32_t p) {
   return f;
 }
 
+int main(void) {
+  float f = 3.1415926, f2;
+  uint32_t netf;
+
+  netf = htonf(f);
+  f2 = ntohf(netf);
+  printf("Original :%f\n", f);
+  printf(" Network: 0x%08X\n", netf);
+  printf("Unpacked: %f\n", f2);
+  return 0;
+}
+
