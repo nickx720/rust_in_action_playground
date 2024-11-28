0c9260741768d98ce2ed2a3c187da9a1253bfa60
Nick <34407800+nickx720@users.noreply.github.com>
2024-11-28 02:58:51 -0500

    Nov28 output correction (#437)
    * completed hton
    
    * wip
    
    ---------
    
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

--- rust-web-development/beej-socket/beej-examples/src/marshall/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/marshall/mod.rs
@@ -1,7 +1,13 @@
 use core::f32;
 
 fn hton(number: f32) -> u32 {
-    let sign = if number > 0.0 { 1u32 } else { 0u32 };
+    let mut number = number;
+    let sign = if number < 0.0 {
+        number = -number;
+        1u32
+    } else {
+        0u32
+    };
     //   p = ((((uint32_t)f) & 0x7fff) << 16) | (sign << 31);
     let mut p = ((number.to_bits() & 0x7fff) << 16u32) | (sign << 31);
     p |= ((number - number.trunc()) * 65536.0f32).to_bits() & 0xffff;
@@ -11,4 +17,6 @@ fn hton(number: f32) -> u32 {
 pub fn marshall() {
     let value = f32::consts::PI;
     let converted_u32 = hton(value);
+    println!("Original {}", value);
+    println!("Network 0x{:08X}", converted_u32);
 }
