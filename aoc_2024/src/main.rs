3c800b12d27954789c9caf956244a4485b5d9197
Nick <34407800+nickx720@users.noreply.github.com>
2024-12-01 10:51:28 -0500

    Aoc 2024 day one (#440)
    * day one
    
    * completed
    
    ---------
    
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro.local>

---
+++ aoc_2024/src/main.rs
@@ -0,0 +1,34 @@
+use clap::{Parser, Subcommand};
+mod day_one;
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    One {
+        part_one: Option<bool>,
+        part_two: Option<bool>,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::One { part_one, part_two }) => {
+            println!("Invoking Day one");
+            let file = "./assets/day_one/question.txt";
+            if part_one.is_some() {
+                let output = day_one::partone::part_one(file).unwrap();
+                println!("The output of day one first part is {}", output);
+            }
+            if part_two.is_some() {
+                let output = day_one::parttwo::part_two(file).unwrap();
+                println!("The output of day one second part is {}", output);
+            }
+        }
+        _ => panic!("Invalid day"),
+    }
+}
