6ba617f548abb48756bce4c1b33eed265cde47dc
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-01-01 09:05:23 -0500

    wp

--- aoc_2024/src/day_twenty_four/part_one.rs
+++ aoc_2024/src/day_twenty_four/part_one.rs
@@ -3,3 +3,13 @@ use std::error::Error;
 pub fn part_one(path: &str) -> Result<usize, Box<dyn Error>> {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn day_twenty_four_part_one() -> Result<(), Box<dyn Error>> {
+        assert_eq!(1, 1);
+        Ok(())
+    }
+}
--- aoc_2024/src/day_twenty_four/part_two.rs
+++ aoc_2024/src/day_twenty_four/part_two.rs
@@ -3,3 +3,13 @@ use std::error::Error;
 pub fn part_two(path: &str) -> Result<usize, Box<dyn Error>> {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn day_twenty_four_part_two() -> Result<(), Box<dyn Error>> {
+        assert_eq!(1, 1);
+        Ok(())
+    }
+}
