7a9120c0e0987e2a0440d6a26b10523091d06efd
Nick <34407800+nickx720@users.noreply.github.com>
2025-01-03 09:17:56 -0500

    part one completed (#473)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- aoc_2024/src/day_twenty_four/mod.rs
+++ aoc_2024/src/day_twenty_four/mod.rs
@@ -1,2 +1,2 @@
-mod part_one;
-mod part_two;
+pub mod part_one;
+pub mod part_two;
--- aoc_2024/src/day_twenty_four/part_one.rs
+++ aoc_2024/src/day_twenty_four/part_one.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::{HashMap, VecDeque},
     error::Error,
+    fs::read_to_string,
 };
 #[derive(Debug, Clone, Eq, Hash, PartialEq)]
 enum GateOp {
@@ -129,8 +130,12 @@ fn compute(wires: &mut HashMap<String, Option<bool>>, gates: &Vec<Gate>) -> Resu
     Ok(())
 }
 
-pub fn part_one(path: &str) -> Result<usize, Box<dyn Error>> {
-    todo!()
+pub fn part_one(path: &str) -> Result<u64, Box<dyn Error>> {
+    let input = read_to_string(path).unwrap();
+    let (mut wires, gates) = parse_input(&input);
+    compute(&mut wires, &gates).unwrap();
+    let decimal = wires_to_decimal(&wires, "z");
+    Ok(decimal)
 }
 
 #[cfg(test)]
@@ -138,7 +143,8 @@ mod tests {
     use super::*;
     #[test]
     fn day_twenty_four_part_one() -> Result<(), Box<dyn Error>> {
-        assert_eq!(1, 1);
+        let output = part_one("./assets/day_twenty_four/sample.txt")?;
+        assert_eq!(output, 2024);
         Ok(())
     }
 }
