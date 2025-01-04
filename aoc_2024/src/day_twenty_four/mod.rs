6c4d9c959ae5dc4790fb608c04e8b32e72364ac1
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-01-04 08:09:04 -0500

    wip

--- aoc_2024/src/day_twenty_four/mod.rs
+++ aoc_2024/src/day_twenty_four/mod.rs
@@ -1,2 +1,43 @@
 pub mod part_one;
 pub mod part_two;
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
+enum GateOp {
+    AND,
+    OR,
+    XOR,
+}
+
+impl GateOp {
+    fn from_str(str: &str) -> GateOp {
+        match str {
+            "AND" => GateOp::AND,
+            "OR" => GateOp::OR,
+            "XOR" => GateOp::XOR,
+            _ => panic!(),
+        }
+    }
+
+    fn apply(&self, a: bool, b: bool) -> bool {
+        match &self {
+            GateOp::AND => a && b,
+            GateOp::OR => a || b,
+            GateOp::XOR => a ^ b,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
+struct Gate {
+    inputs: (String, String),
+    output: String,
+    op: GateOp,
+}
+
+impl Gate {
+    fn print(&self) {
+        print!(
+            "{} {:?} {} -> {}",
+            self.inputs.0, self.op, self.inputs.1, self.output
+        );
+    }
+}
