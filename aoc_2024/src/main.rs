14d2720d52d3e795fd25bdc534eb80d56f421a47
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-01-08 08:28:43 -0500

    main changes

--- aoc_2024/src/main.rs
+++ aoc_2024/src/main.rs
@@ -665,6 +665,28 @@ fn main() {
                 _ => println!("Ignoring day twenty_four part two"),
             }
         }
+        Some(Commands::TwentyFive { part_one, part_two }) => {
+            println!("Invoking Day TwentyFive");
+            let file = "./assets/day_twenty_five/question.txt";
+            match part_one {
+                Some(value) => {
+                    if *value {
+                        let output = day_twenty_five::part_one::part_one(file).unwrap();
+                        println!("The output of day twenty_five first part is {}", output);
+                    }
+                }
+                _ => println!("Ignoring day twenty_five part one"),
+            }
+            match part_two {
+                Some(value) => {
+                    if *value {
+                        let output = day_twenty_five::part_two::part_two(file).unwrap();
+                        println!("The output of day twenty_five second part is {}", output);
+                    }
+                }
+                _ => println!("Ignoring day twenty_five part two"),
+            }
+        }
         _ => panic!("Invalid day"),
     }
 }
