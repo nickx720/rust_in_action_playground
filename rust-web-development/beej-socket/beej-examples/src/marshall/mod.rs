04d015f6d1fb8e4921e362901f02cf482d625f12
Nick <34407800+nickx720@users.noreply.github.com>
2025-01-16 06:40:39 -0500

    wip (#483)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-web-development/beej-socket/beej-examples/src/marshall/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/marshall/mod.rs
@@ -8,10 +8,23 @@ fn hton(number: f32) -> u32 {
     } else {
         0u32
     };
-    //   p = ((((uint32_t)f) & 0x7fff) << 16) | (sign << 31);
+    //    p = ((((uint32_t)f) & 0x7fff) << 16) | (sign << 31);
     let mut p = (((number as u32) & 0x7fff) << 16u32) | (sign << 31);
     p |= (((number - number.trunc()) * 65536.0f32) as u32) & 0xffff;
     p
+}
+
+fn ntoh(number: u32) -> f32 {
+    // float f = ((p >> 16) & 0x7fff);
+    // f += (p & 0xffff) / 65536.0f;
+
+    // if (((p >> 31) & 0x1) == 0x1) {
+    //   f = -1;
+    // }
+    // return f;
+    let mut float: f32 = ((number >> 16) & 0x7fff) as f32;
+    float += (number & 0xffff) as f32 / 65536.0f32;
+    todo!()
 }
 
 pub fn marshall() {
