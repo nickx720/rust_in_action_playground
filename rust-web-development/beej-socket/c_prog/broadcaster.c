eb7153db1f94327de891c307ee6c6d37630a8701
Nick <34407800+nickx720@users.noreply.github.com>
2025-01-21 02:43:02 -0500

    wip (#488)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

---
+++ rust-web-development/beej-socket/c_prog/broadcaster.c
@@ -0,0 +1,15 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define SERVERPORT 4950
+
+int main(int argc, char *argv[]) {}
+
