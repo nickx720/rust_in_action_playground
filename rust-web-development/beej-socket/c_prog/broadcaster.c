8fd726e1c353546300fc0e7c0d1fdfedced0a4b6
Nick <34407800+nickx720@users.noreply.github.com>
2025-01-22 02:48:05 -0500

    wip (#489)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-web-development/beej-socket/c_prog/broadcaster.c
+++ rust-web-development/beej-socket/c_prog/broadcaster.c
@@ -11,5 +11,26 @@
 
 #define SERVERPORT 4950
 
-int main(int argc, char *argv[]) {}
+int main(int argc, char *argv[]) {
+  int sockfd;
+  struct sockaddr_in their_addr;
+  struct hostent *he;
+  int numbytes;
+  int broadcast = 1;
+
+  if (argc != 3) {
+    fprintf(stderr, "usage: broadcaster hostname message\n");
+    exit(1);
+  }
+
+  if ((he = gethostbyname(argv[1])) == NULL) {
+    perror("gethostbyname");
+    exit(1);
+  }
+
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+    perror("socket");
+    exit(1);
+  }
+}
 
