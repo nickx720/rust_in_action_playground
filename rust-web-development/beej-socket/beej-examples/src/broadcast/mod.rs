57ac97b0cf13ab6689519fba39c67ed7229fdfcc
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-01-24 05:14:55 -0500

    wip

---
+++ rust-web-development/beej-socket/beej-examples/src/broadcast/mod.rs
@@ -0,0 +1,5 @@
+use std::net::IpAddr;
+
+pub fn socketbroadcaster(host: IpAddr, port: u16, message: String) {
+    todo!()
+}
--- rust-web-development/beej-socket/beej-examples/src/lib.rs
+++ rust-web-development/beej-socket/beej-examples/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod broadcast;
 pub mod listener;
 pub mod marshall;
 pub mod pollexample;
