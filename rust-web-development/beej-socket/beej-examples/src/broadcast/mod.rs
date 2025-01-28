37d2449740c524752682cf2894a2e472f34da0c3
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-01-28 02:11:13 -0500

    wip

--- rust-web-development/beej-socket/beej-examples/src/broadcast/mod.rs
+++ rust-web-development/beej-socket/beej-examples/src/broadcast/mod.rs
@@ -1,9 +1,20 @@
 use std::net::{IpAddr, SocketAddrV4, SocketAddrV6};
 
+use nix::unistd;
+
 pub fn socketbroadcaster(host: IpAddr, port: u16, message: String) {
     match host {
         IpAddr::V4(addr) => {
             let socket = SocketAddrV4::new(addr, port);
+            let hostname = unistd::gethostname().unwrap().into_string().unwrap();
+            let socket: nix::sys::socket::SockaddrIn = socket.into();
+            let sockfd = nix::sys::socket::socket(
+                nix::sys::socket::AddressFamily::Inet,
+                nix::sys::socket::SockType::Datagram,
+                nix::sys::socket::SockFlag::empty(),
+                None,
+            )
+            .expect("Failed to create sockfd");
         }
         IpAddr::V6(addr) => {
             let socket = SocketAddrV6::new(addr, port, 0, 0);
