cf03906809ab4867c1e1d9a11d8d86779c87708c
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-02-06 03:51:01 -0500

    wip

--- rust-for-rustaceans/foundations/src/main.rs
+++ rust-for-rustaceans/foundations/src/main.rs
@@ -4,4 +4,12 @@ fn main() {
     let mut var2 = &x;
     var2 = &y;
     dbg!(*var2);
+    let x1 = 42;
+    let y1 = Box::new(84);
+    {
+        let z = (x1, y1);
+        // z goes out scope
+    }
+    let x2 = x1;
+    //    let y2 = y1; won't work since y1 no longer owns the pointer
 }
