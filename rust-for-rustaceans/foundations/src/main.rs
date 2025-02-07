66456d6abbac9fbab48c246ab223c863965ba66b
Nick <34407800+nickx720@users.noreply.github.com>
2025-02-07 05:02:00 -0500

    shared references (#505)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-for-rustaceans/foundations/src/main.rs
+++ rust-for-rustaceans/foundations/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
     let x = 42;
     let y = 43;
-    let mut var2 = &x;
-    var2 = &y;
-    dbg!(*var2);
+    let mut var2 = x;
+    // var2 = &y;
     let x1 = 42;
     let y1 = Box::new(84);
     {
@@ -12,4 +11,10 @@ fn main() {
     }
     let x2 = x1;
     //    let y2 = y1; won't work since y1 no longer owns the pointer
+    cache(&84, &mut var2);
+}
+
+fn cache(input: &i32, sum: &mut i32) {
+    *sum = *input + *input;
+    assert_eq!(*sum, 2 * *input);
 }
