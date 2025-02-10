3e24ccc88537ab00e81419c4de914f5b1fec375f
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-02-10 02:14:58 -0500

    mutable references

--- rust-for-rustaceans/foundations/src/main.rs
+++ rust-for-rustaceans/foundations/src/main.rs
@@ -12,9 +12,20 @@ fn main() {
     let x2 = x1;
     //    let y2 = y1; won't work since y1 no longer owns the pointer
     cache(&84, &mut var2);
+    let mut s = Box::new(42);
+    replace_with_84(&mut s);
 }
 
 fn cache(input: &i32, sum: &mut i32) {
     *sum = *input + *input;
     assert_eq!(*sum, 2 * *input);
+}
+
+fn replace_with_84(s: &mut Box<i32>) {
+    let was = std::mem::take(s);
+    *s = was;
+    let mut r = Box::new(84);
+    std::mem::swap(s, &mut r);
+    assert_eq!(*r, 42);
+    assert_ne!(*r, 84);
 }
