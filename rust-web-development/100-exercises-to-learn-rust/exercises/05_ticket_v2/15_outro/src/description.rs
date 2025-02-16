0ffc72e1b109e5c8adcdca3b39ee7ba3475544f9
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-02-16 03:08:41 -0500

    completed description

--- rust-web-development/100-exercises-to-learn-rust/exercises/05_ticket_v2/15_outro/src/description.rs
+++ rust-web-development/100-exercises-to-learn-rust/exercises/05_ticket_v2/15_outro/src/description.rs
@@ -2,7 +2,36 @@
 //   enforcing that the description is not empty and is not longer than 500 bytes.
 //   Implement the traits required to make the tests pass too.
 
+#[derive(Debug, thiserror::Error)]
+enum TicketDescriptionError {
+    #[error("The description cannot be empty")]
+    DescriptionCannotBeEmpty,
+    #[error("The description cannot be longer than 500 bytes")]
+    DescriptionCannotLongerThan500bytes,
+}
+
+#[derive(Debug)]
 pub struct TicketDescription(String);
+
+impl TryFrom<String> for TicketDescription {
+    type Error = TicketDescriptionError;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            return Err(TicketDescriptionError::DescriptionCannotBeEmpty);
+        }
+        if value.len() > 500 {
+            return Err(TicketDescriptionError::DescriptionCannotLongerThan500bytes);
+        }
+        Ok(TicketDescription(value))
+    }
+}
+
+impl TryFrom<&str> for TicketDescription {
+    type Error = TicketDescriptionError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        TicketDescription::try_from(value.to_string())
+    }
+}
 
 #[cfg(test)]
 mod tests {
