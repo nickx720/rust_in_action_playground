40cf16c44c5dbd053c34404ed60c41ca8b753ec0
Nick <34407800+nickx720@users.noreply.github.com>
2025-02-19 02:36:59 -0500

    wip (#517)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-for-rustaceans/types/src/main.rs
+++ rust-for-rustaceans/types/src/main.rs
@@ -1,7 +1,19 @@
+use std::{u32, u64, u8};
+
 fn main() {
-    println!("Hello, world!");
+    let foo = Foo {
+        tiny: false,
+        normal: u32::MAX,
+        small: u8::MAX,
+        long: u64::MAX,
+        short: u16::MAX,
+    };
+    dbg!("{}", std::mem::size_of::<Foo>());
 }
-#[repr(C)]
+// #[repr(align(1024))] 1024 bytes
+// #[repr(packed)] 16 bytes
+#[repr(C)] // 32 bytes with padding
+#[derive(Debug)]
 struct Foo {
     tiny: bool,
     normal: u32,
