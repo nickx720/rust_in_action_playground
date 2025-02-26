5a621bbad3310bdff111de876836395558453a6a
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-02-26 01:46:23 -0500

    working with sqlite

--- rust-web-development/data-privacy/src/main.rs
+++ rust-web-development/data-privacy/src/main.rs
@@ -1,6 +1,11 @@
 use std::collections::HashMap;
 
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web::Json};
+use actix_web::{
+    App, HttpResponse, HttpServer, Responder, get, middleware, post,
+    web::{self, Json},
+};
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -25,8 +30,15 @@ async fn detokenize(req_body: String) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
+    let manager = SqliteConnectionManager::file("dataprivacy.db");
+    let pool = Pool::new(manager).unwrap();
+
+    log::info!("starting server at http://localhost:8080");
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .wrap(middleware::Logger::default())
             .service(tokenize)
             .service(hello)
             .service(detokenize)
