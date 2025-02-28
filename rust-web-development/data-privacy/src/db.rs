3fea47bd14c36d487da4d97296e63127adcef889
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-02-28 02:32:39 -0500

    wip

--- rust-web-development/data-privacy/src/db.rs
+++ rust-web-development/data-privacy/src/db.rs
@@ -1,6 +1,7 @@
 pub type Pool = r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>;
 pub type Connection = r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager>;
-use actix_web::Error;
+use actix_web::{Error, error, web};
+use rusqlite::params;
 
 use crate::{Deserialize, Serialize};
 
@@ -11,8 +12,43 @@ pub struct DataPrivacyStore {
     token: String,
 }
 
-pub enum Queries {}
+type DataPrivacyStoreError = Result<Vec<DataPrivacyStore>, rusqlite::Error>;
 
-pub async fn execute(pool: &Pool, query: Queries) -> Result<Vec<DataPrivacyStore>, Error> {
+pub enum Queries {
+    InsertTokens,
+}
+
+pub async fn execute(
+    pool: &Pool,
+    query: Queries,
+    values: DataPrivacyStore,
+) -> Result<Vec<DataPrivacyStore>, Error> {
+    let pool = pool.clone();
+    let conn = web::block(move || pool.get())
+        .await?
+        .map_err(error::ErrorInternalServerError)?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS value (
+            id INTEGER PRIMARY KEY,
+            original TEXT NOT NULL,
+            token INTEGER NOT NULL
+        )",
+        [],
+    )
+    .unwrap();
+    web::block(move || match query {
+        Queries::InsertTokens => insert_token(conn, values),
+    })
+    .await?
+    .map_err(error::ErrorInternalServerError)
+}
+
+fn insert_token(conn: Connection, values: DataPrivacyStore) -> DataPrivacyStoreError {
+    let stmt = conn.execute(
+        "
+INSERT into vault (id,original,token) VALUES (?1,?2,?3)
+",
+        params![values.id, values.original, values.token],
+    )?;
     todo!()
 }
