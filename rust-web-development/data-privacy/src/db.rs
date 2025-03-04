8333cf7cbdc3bec860d3a45331ab658330cc4301
Nick <34407800+nickx720@users.noreply.github.com>
2025-03-04 01:39:37 -0500

    wip (#530)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-web-development/data-privacy/src/db.rs
+++ rust-web-development/data-privacy/src/db.rs
@@ -12,27 +12,6 @@ pub struct DataPrivacyStore {
     token: String,
 }
 
-type DataPrivacyStoreError = Result<Vec<DataPrivacyStore>, rusqlite::Error>;
-
-pub enum Queries {
-    InsertTokens,
-}
-
-pub async fn execute(
-    pool: &Pool,
-    query: Queries,
-    values: DataPrivacyStore,
-) -> Result<Vec<DataPrivacyStore>, Error> {
-    let pool = pool.clone();
-    let conn = web::block(move || pool.get())
-        .await?
-        .map_err(error::ErrorInternalServerError)?;
-    web::block(move || match query {
-        Queries::InsertTokens => insert_token(conn, values),
-    })
-    .await?
-    .map_err(error::ErrorInternalServerError)
-}
 pub async fn initialize_db(pool: &Pool) -> Result<(), Error> {
     let pool = pool.clone();
     let conn = web::block(move || pool.get())
@@ -49,13 +28,17 @@ pub async fn initialize_db(pool: &Pool) -> Result<(), Error> {
     .unwrap();
     Ok(())
 }
-
-fn insert_token(conn: Connection, values: DataPrivacyStore) -> DataPrivacyStoreError {
+// TODO handle error
+pub async fn insert_token(pool: &Pool, values: DataPrivacyStore) -> Result<usize, Error> {
+    let pool = pool.clone();
+    let conn = web::block(move || pool.get())
+        .await?
+        .map_err(error::ErrorInternalServerError)?;
     let stmt = conn.execute(
         "
 INSERT into vault (id,original,token) VALUES (?1,?2,?3)
 ",
         params![values.id, values.original, values.token],
     )?;
-    todo!()
+    Ok(stmt)
 }
