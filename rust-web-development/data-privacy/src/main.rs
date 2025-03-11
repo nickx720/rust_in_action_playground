a174f1a13864488cd11a796e400658d3462e267a
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-03-11 01:59:54 -0400

    wip

--- rust-web-development/data-privacy/src/main.rs
+++ rust-web-development/data-privacy/src/main.rs
@@ -6,14 +6,14 @@ use actix_web::{
 };
 use aes_gcm::{
     Aes256Gcm, Key, Nonce,
-    aead::{AeadCore, KeyInit, OsRng},
+    aead::{Aead, AeadCore, KeyInit, OsRng},
 };
 use rand::Rng;
 mod db;
 use db::{DataPrivacyStore, Pool, initialize_db, insert_token};
 use r2d2_sqlite::SqliteConnectionManager;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, io::Read};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct RequestPayload {
@@ -26,16 +26,16 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-fn encrypt_data(data: &str, key: &[u8; 32]) -> Vec<u8> {
+fn encrypt_data(data: &str, key: &[u8; 32]) -> (Vec<u8>, Vec<u8>) {
     let key = Key::<Aes256Gcm>::from_slice(key);
     let cipher = Aes256Gcm::new(&key);
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
     let ciphertext = cipher.encrypt(&nonce, data.as_bytes()).unwrap();
-    [nonce.to_vec(), ciphertext].concat()
+    (nonce.to_vec(), ciphertext)
 }
 
 fn decrypt_data(encrypted_data: &[u8], key: &[u8; 32]) -> String {
-    let cipher = Aes256Gcm::new(Key::from_slice(key));
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key));
     let (nonce, ciphertext) = encrypted_data.split_at(12); // Extract nonce
     let plaintext = cipher
         .decrypt(Nonce::from_slice(nonce), ciphertext)
@@ -44,14 +44,20 @@ fn decrypt_data(encrypted_data: &[u8], key: &[u8; 32]) -> String {
 }
 
 #[post("/tokenize")]
-async fn tokenize(req_body: Json<RequestPayload>, pool: web::Data<Pool>) -> impl Responder {
+async fn tokenize(
+    req_body: Json<RequestPayload>,
+    pool: web::Data<Pool>,
+    key: web::Data<[u8; 32]>,
+) -> impl Responder {
     let original = serde_json::to_string(&req_body.data).unwrap();
-    for (_key, tokenize) in req_body.data.to_owned().iter_mut() {}
-    let token = DataPrivacyStore::new(
-        req_body.id.parse::<u32>().unwrap(),
-        original,
-        "no".to_string(),
-    );
+    let mut nonce = Vec::new();
+    for (_index, tokenize) in req_body.data.to_owned().iter_mut() {
+        let token = encrypt_data(&tokenize, &key);
+        nonce = token.0;
+        *tokenize = String::from_utf8(token.1).expect("Conversion failed");
+    }
+    let token = serde_json::to_string(&req_body.data).unwrap();
+    let token = DataPrivacyStore::new(req_body.id.parse::<u32>().unwrap(), original, token, nonce);
     match insert_token(&pool, token).await {
         Ok(val) => HttpResponse::Ok().body(val.to_string()),
         Err(e) => {
