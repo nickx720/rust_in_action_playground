03119744eb08572bba82da8516968449e8dc08ec
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-03-13 04:41:45 -0400

    encrypt working with nonce

--- rust-web-development/data-privacy/src/main.rs
+++ rust-web-development/data-privacy/src/main.rs
@@ -14,7 +14,7 @@ use base64::prelude::*;
 use db::{DataPrivacyStore, Pool, initialize_db, insert_token};
 use r2d2_sqlite::SqliteConnectionManager;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, io::Read};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct RequestPayload {
@@ -27,12 +27,12 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-fn encrypt_data(data: &str, key: &[u8; 32]) -> (Vec<u8>, Vec<u8>) {
+fn encrypt_data(data: &str, key: &[u8; 32]) -> Vec<u8> {
     let key = Key::<Aes256Gcm>::from_slice(key);
     let cipher = Aes256Gcm::new(&key);
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
     let ciphertext = cipher.encrypt(&nonce, data.as_bytes()).unwrap();
-    (nonce.to_vec(), ciphertext)
+    [nonce.to_vec(), ciphertext].concat()
 }
 
 fn decrypt_data(encrypted_data: &[u8], key: &[u8; 32]) -> String {
@@ -51,16 +51,18 @@ async fn tokenize(
     key: web::Data<[u8; 32]>,
 ) -> impl Responder {
     let original = serde_json::to_string(&req_body.data).unwrap();
-    let mut nonce = Vec::new();
-    for (_index, tokenize) in req_body.data.to_owned().iter_mut() {
-        let token = encrypt_data(&tokenize, &key);
-        nonce = token.0;
-        let string = BASE64_STANDARD.encode(token.1);
-        dbg!(&string);
-        *tokenize = string;
-    }
-    let token = serde_json::to_string(&req_body.data).unwrap();
-    let token = DataPrivacyStore::new(req_body.id.parse::<u32>().unwrap(), original, token, nonce);
+    let token = req_body
+        .data
+        .iter()
+        .map(|(index, tokenize)| {
+            let token = encrypt_data(tokenize, &key);
+            let string = BASE64_STANDARD.encode(token);
+            (index.clone(), string)
+        })
+        .collect::<HashMap<String, String>>();
+    let token = serde_json::to_string(&token).unwrap();
+    dbg!(&token);
+    let token = DataPrivacyStore::new(req_body.id.parse::<u32>().unwrap(), original, token);
     match insert_token(&pool, token).await {
         Ok(val) => HttpResponse::Ok().body(val.to_string()),
         Err(e) => {
