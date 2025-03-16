6151781402c80f530eac6deee9856477a1f9b50f
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-03-16 02:48:42 -0400

    wip

--- rust-web-development/100-exercises-to-learn-rust/exercises/06_ticket_management/05_iter/src/lib.rs
+++ rust-web-development/100-exercises-to-learn-rust/exercises/06_ticket_management/05_iter/src/lib.rs
@@ -5,7 +5,11 @@ use ticket_fields::{TicketDescription, TicketTitle};
 pub struct TicketStore {
     tickets: Vec<Ticket>,
 }
-
+impl TicketStore {
+    pub fn iter(&self) -> std::slice::Iter<Ticket> {
+        self.tickets.iter()
+    }
+}
 #[derive(Debug, Clone, PartialEq)]
 pub struct Ticket {
     title: TicketTitle,
