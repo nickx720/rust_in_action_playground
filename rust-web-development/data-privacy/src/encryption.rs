9b3f416266a5376ba3c6d461b7df65866c39fead
Nick <34407800+nickx720@users.noreply.github.com>
2025-03-26 02:13:10 -0400

    wip (#552)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-web-development/data-privacy/src/encryption.rs
+++ rust-web-development/data-privacy/src/encryption.rs
@@ -1,18 +1,23 @@
 use aes_gcm::{
-    Aes256Gcm, Key, Nonce,
+    Aes256Gcm, Error as AesGcmError, Key, Nonce,
     aead::{Aead, AeadCore, KeyInit, OsRng},
 };
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum EncryptionError {}
+pub enum EncryptionError {
+    #[error("Encryption Fail")]
+    Encrypt(AesGcmError),
+}
 
-pub fn encrypt_data(data: &str, key: &[u8; 32]) -> Vec<u8> {
+pub fn encrypt_data(data: &str, key: &[u8; 32]) -> Result<Vec<u8>, EncryptionError> {
     let key = Key::<Aes256Gcm>::from_slice(key);
     let cipher = Aes256Gcm::new(&key);
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
-    let ciphertext = cipher.encrypt(&nonce, data.as_bytes()).unwrap();
-    [nonce.to_vec(), ciphertext].concat()
+    let ciphertext = cipher
+        .encrypt(&nonce, data.as_bytes())
+        .map_err(EncryptionError::Encrypt)?;
+    Ok([nonce.to_vec(), ciphertext].concat())
 }
 
 pub fn decrypt_data(encrypted_data: &[u8], key: &[u8; 32]) -> String {
