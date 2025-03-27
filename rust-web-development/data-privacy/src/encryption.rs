cccf39382eed6e77fa1f44bbb24078eda6ffbea6
Nick <34407800+nickx720@users.noreply.github.com>
2025-03-27 03:08:10 -0400

    wip (#553)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-web-development/data-privacy/src/encryption.rs
+++ rust-web-development/data-privacy/src/encryption.rs
@@ -1,3 +1,5 @@
+use std::string::FromUtf8Error;
+
 use aes_gcm::{
     Aes256Gcm, Error as AesGcmError, Key, Nonce,
     aead::{Aead, AeadCore, KeyInit, OsRng},
@@ -8,6 +10,8 @@ use thiserror::Error;
 pub enum EncryptionError {
     #[error("Encryption Fail")]
     Encrypt(AesGcmError),
+    #[error("String Conversion Failed")]
+    StringConvFailed(FromUtf8Error),
 }
 
 pub fn encrypt_data(data: &str, key: &[u8; 32]) -> Result<Vec<u8>, EncryptionError> {
@@ -20,11 +24,11 @@ pub fn encrypt_data(data: &str, key: &[u8; 32]) -> Result<Vec<u8>, EncryptionErr
     Ok([nonce.to_vec(), ciphertext].concat())
 }
 
-pub fn decrypt_data(encrypted_data: &[u8], key: &[u8; 32]) -> String {
+pub fn decrypt_data(encrypted_data: &[u8], key: &[u8; 32]) -> Result<String, EncryptionError> {
     let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key));
     let (nonce, ciphertext) = encrypted_data.split_at(12); // Extract nonce
     let plaintext = cipher
         .decrypt(Nonce::from_slice(nonce), ciphertext)
-        .unwrap();
-    String::from_utf8(plaintext).unwrap()
+        .map_err(EncryptionError::Encrypt)?;
+    String::from_utf8(plaintext)?
 }
