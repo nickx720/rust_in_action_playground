346e3428442194becf9d0aaa8cd172386a1cd363
Nick <34407800+nickx720@users.noreply.github.com>
2025-05-03 07:02:14 -0400

    completed btreemap (#589)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-web-development/100-exercises-to-learn-rust/exercises/06_ticket_management/16_btreemap/src/lib.rs
+++ rust-web-development/100-exercises-to-learn-rust/exercises/06_ticket_management/16_btreemap/src/lib.rs
@@ -12,8 +12,16 @@ pub struct TicketStore {
     tickets: BTreeMap<TicketId, Ticket>,
     counter: u64,
 }
+impl<'a> IntoIterator for &'a TicketStore {
+    type Item = &'a Ticket;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+    fn into_iter(self) -> Self::IntoIter {
+        let tickets = self.tickets.values().collect::<Vec<&Ticket>>();
+        tickets.into_iter()
+    }
+}
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Ord, Eq)]
 pub struct TicketId(u64);
 
 #[derive(Clone, Debug, PartialEq)]
@@ -40,7 +48,7 @@ pub enum Status {
 impl TicketStore {
     pub fn new() -> Self {
         Self {
-            tickets: todo!(),
+            tickets: BTreeMap::new(),
             counter: 0,
         }
     }
@@ -54,16 +62,16 @@ impl TicketStore {
             description: ticket.description,
             status: Status::ToDo,
         };
-        todo!();
+        self.tickets.insert(id, ticket);
         id
     }
 
     pub fn get(&self, id: TicketId) -> Option<&Ticket> {
-        todo!()
+        self.tickets.get(&id)
     }
 
     pub fn get_mut(&mut self, id: TicketId) -> Option<&mut Ticket> {
-        todo!()
+        self.tickets.get_mut(&id)
     }
 }
 
