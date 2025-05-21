f0a654098966dddfeb72fd34cac030fa861d26a0
Nick <34407800+nickx720@users.noreply.github.com>
2025-05-21 03:07:49 -0400

    chore: wip reading filelist (#608)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-web-development/password-cracker/src/main.rs
+++ rust-web-development/password-cracker/src/main.rs
@@ -1,10 +1,29 @@
 use brute::crack;
 use md5::md5;
+use wordlist::wordlist_reader;
 
 mod brute;
 mod md5;
 
+mod wordlist {
+    //In this step your goal is to use a word list to speed up the attack. Instead of generated every single possible permutation of letters we’ll use a word list of common passwords. You can get one such list from CrackStation here. Grab the Smaller Wordlist for now.
+
+    use std::{error::Error, fs::read};
+
+    //Adapt your program to allow the user to specify whether to brute force or use a word list, allowing them to specify the path to the word list. See how quickly you can crack this hash: 2bdb742fc3d075ec6b73ea414f27819a
+    pub fn wordlist_reader() -> Result<String, Box<dyn Error>> {
+        let file = read("assets/realhuman_phill.txt")?;
+        let contents = file
+            .iter()
+            .map(|item| item.to_string())
+            .collect::<Vec<String>>();
+        dbg!(contents.len());
+        todo!()
+    }
+}
+
 fn main() {
     md5("abc".to_string());
     crack("pass".to_uppercase().to_string());
+    let _ = wordlist_reader().unwrap();
 }
