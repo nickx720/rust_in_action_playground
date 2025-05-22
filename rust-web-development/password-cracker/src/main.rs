b26eded030d5c71e58675f421a2c9687a6a98a70
Nick <34407800+nickx720@users.noreply.github.com>
2025-05-22 03:07:38 -0400

    chore: wip printing out utf8 content (#609)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-web-development/password-cracker/src/main.rs
+++ rust-web-development/password-cracker/src/main.rs
@@ -8,16 +8,20 @@ mod md5;
 mod wordlist {
     //In this step your goal is to use a word list to speed up the attack. Instead of generated every single possible permutation of letters we’ll use a word list of common passwords. You can get one such list from CrackStation here. Grab the Smaller Wordlist for now.
 
-    use std::{error::Error, fs::read};
+    use std::{error::Error, f32::consts::LN_2, fs::read};
 
     //Adapt your program to allow the user to specify whether to brute force or use a word list, allowing them to specify the path to the word list. See how quickly you can crack this hash: 2bdb742fc3d075ec6b73ea414f27819a
     pub fn wordlist_reader() -> Result<String, Box<dyn Error>> {
         let file = read("assets/realhuman_phill.txt")?;
-        let contents = file
-            .iter()
-            .map(|item| item.to_string())
-            .collect::<Vec<String>>();
-        dbg!(contents.len());
+        let content: Vec<String> = String::from_utf8_lossy(&file)
+            .split("\n")
+            .take(10)
+            .map(|item| item.to_owned())
+            .collect();
+        for item in content {
+            // TODO confirm if the printed output is valid to compare
+            dbg!(&item);
+        }
         todo!()
     }
 }
