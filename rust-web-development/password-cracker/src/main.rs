636e7f6824269555ab31fc17eccd83136c3bfb16
Nick <34407800+nickx720@users.noreply.github.com>
2025-05-23 06:36:05 -0400

    feat: adding build script to initialize sqlite3 (#610)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-web-development/password-cracker/src/main.rs
+++ rust-web-development/password-cracker/src/main.rs
@@ -8,7 +8,7 @@ mod md5;
 mod wordlist {
     //In this step your goal is to use a word list to speed up the attack. Instead of generated every single possible permutation of letters we’ll use a word list of common passwords. You can get one such list from CrackStation here. Grab the Smaller Wordlist for now.
 
-    use std::{error::Error, f32::consts::LN_2, fs::read};
+    use std::{error::Error, fs::read};
 
     //Adapt your program to allow the user to specify whether to brute force or use a word list, allowing them to specify the path to the word list. See how quickly you can crack this hash: 2bdb742fc3d075ec6b73ea414f27819a
     pub fn wordlist_reader() -> Result<String, Box<dyn Error>> {
@@ -20,7 +20,7 @@ mod wordlist {
             .collect();
         for item in content {
             // TODO confirm if the printed output is valid to compare
-            dbg!(&item);
+            println!("{}", item);
         }
         todo!()
     }
