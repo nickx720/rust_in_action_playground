736f0359264b55806462c0eb89ebf8e7ff6d2156
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-06-01 06:18:55 -0400

    feat: creating abuilder func

--- rust-web-development/100-exercises-to-learn-rust/exercises/07_threads/08_client/src/lib.rs
+++ rust-web-development/100-exercises-to-learn-rust/exercises/07_threads/08_client/src/lib.rs
@@ -7,23 +7,46 @@ pub mod store;
 
 #[derive(Clone)]
 // TODO: flesh out the client implementation.
-pub struct TicketStoreClient {}
+pub struct TicketStoreClient {
+    sender: Sender<Command>,
+}
 
 impl TicketStoreClient {
     // Feel free to panic on all errors, for simplicity.
     pub fn insert(&self, draft: TicketDraft) -> TicketId {
-        todo!()
+        let (response_sender, response_receiver) = std::sync::mpsc::channel();
+        let command = Command::Insert {
+            draft: draft.clone(),
+            response_channel: response_sender,
+        };
+
+        self.sender
+            .send(command)
+            // If the thread is no longer running, this will panic
+            // because the channel will be closed.
+            .expect("Did you actually spawn a thread? The channel is closed!");
+        let ticket_id: TicketId = response_receiver.recv().expect("No response received!");
+        ticket_id
     }
 
     pub fn get(&self, id: TicketId) -> Option<Ticket> {
-        todo!()
+        let (response_sender, response_receiver) = std::sync::mpsc::channel();
+        let command = Command::Get {
+            id: id,
+            response_channel: response_sender,
+        };
+        self.sender
+            .send(command)
+            .expect("Did you actually spawn a thread? The channel is closed!");
+
+        response_receiver.recv().expect("Value not there")
     }
 }
 
 pub fn launch() -> TicketStoreClient {
     let (sender, receiver) = std::sync::mpsc::channel();
     std::thread::spawn(move || server(receiver));
-    todo!()
+    TicketStoreClient { sender }
 }
 
 // No longer public! This becomes an internal detail of the library now.
