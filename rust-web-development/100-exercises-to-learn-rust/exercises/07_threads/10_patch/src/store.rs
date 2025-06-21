c8d2bc528e0d9512bb5d49cee20d930ce19d4784
Nick <34407800+nickx720@users.noreply.github.com>
2025-06-21 11:51:37 -0400

    feat: completed patch (#639)
    Co-authored-by: Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>

--- rust-web-development/100-exercises-to-learn-rust/exercises/07_threads/10_patch/src/store.rs
+++ rust-web-development/100-exercises-to-learn-rust/exercises/07_threads/10_patch/src/store.rs
@@ -1,4 +1,4 @@
-use crate::data::{Status, Ticket, TicketDraft};
+use crate::data::{Status, Ticket, TicketDraft, TicketPatch};
 use std::collections::BTreeMap;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
@@ -37,5 +37,34 @@ impl TicketStore {
 
     pub fn get_mut(&mut self, id: TicketId) -> Option<&mut Ticket> {
         self.tickets.get_mut(&id)
+    }
+    pub fn update_mut(&mut self, id: TicketId, patch: TicketPatch) -> Result<(), String> {
+        let ticket = self.tickets.get_mut(&id);
+        if let Some(ticket) = ticket {
+            let title;
+            let description;
+            let status;
+            if let Some(old_title) = patch.title {
+                title = old_title;
+            } else {
+                title = ticket.title.clone();
+            }
+            if let Some(old_desc) = patch.description {
+                description = old_desc;
+            } else {
+                description = ticket.description.clone();
+            }
+            if let Some(old_status) = patch.status {
+                status = old_status;
+            } else {
+                status = ticket.status;
+            }
+            ticket.title = title;
+            ticket.description = description;
+            ticket.status = status;
+            Ok(())
+        } else {
+            return Err("Not found".to_string());
+        }
     }
 }
