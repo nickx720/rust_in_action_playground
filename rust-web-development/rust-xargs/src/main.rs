75f971de9e387e24e33a1ecf7ab35cac274e384d
Nick Thomas <nickthomasmbp@Nicks-MacBook-Pro-2.local>
2025-06-26 04:26:35 -0400

    chore: wip figuring out clap

--- rust-web-development/rust-xargs/src/main.rs
+++ rust-web-development/rust-xargs/src/main.rs
@@ -18,8 +18,6 @@
 //This is file 2
 //This is file 3
 
-use std::path::PathBuf;
-
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
@@ -28,13 +26,8 @@ struct Cli {
     /// Optional name to operate on
     name: Option<String>,
 
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
     /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
+    debug: Option<u8>,
 
     #[command(subcommand)]
     command: Option<Commands>,
@@ -58,17 +51,14 @@ fn main() {
         println!("Value for name: {name}");
     }
 
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-
     // You can see how many times a particular flag or argument occurred
     // Note, only flags can have multiple occurrences
     match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
+        Some(val) => match val {
+            0 => println!("Yahoo"),
+            _ => println!("Non yahoo"),
+        },
+        None => println!("Don't be crazy"),
     }
 
     // You can check for the existence of subcommands, and if found use their
