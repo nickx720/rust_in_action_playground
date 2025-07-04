63aec8b746522cf8c3bea6ec614897f526d43ef4
Nick <nicktomthomas@gmail.com>
2025-07-04 03:39:12 -0400

    feat: step 1 completed

--- rust-web-development/rust-xargs/src/main.rs
+++ rust-web-development/rust-xargs/src/main.rs
@@ -1,23 +1,3 @@
-// In this step your goal is to build the command ccxargs that will take a whitespace separated set of strings from standard in and convert them into command line arguments that can be passed to a command (referred to as utility in the man page quoted above).
-
-//You can test your code using, this command below to create three text files we can use for testing:
-//
-//% for i in {1..3}; do echo "This is file ${i}" > test-${i}.txt; done;
-//
-//Then in the same directory we can use ls to create a whitespace separated list of files and pipe that into our ccxargs program which we will tell to run the command cat with each of the items in the list as the argument to cat:
-//
-//% ls | ccxargs cat
-//This is file 1
-//This is file 2
-//This is file 3
-//
-//This is the equivalent of having done:
-//
-//% cat test-1.txt test-2.txt test-3.txt
-//This is file 1
-//This is file 2
-//This is file 3
-
 use std::{io::Read, process::Command};
 
 use clap::Parser;
@@ -36,15 +16,16 @@ fn main() {
     let cli = Cli::parse();
 
     // You can check the value provided by positional arguments, or option arguments
+
     if let Some(cmd) = cli.command.as_deref() {
         for command in cmd {
             for item in &stdin_input {
                 // dbg!("I am ", command, item);
 
-                Command::new("sh")
+                let _ = Command::new("sh")
                     .arg("-c")
                     .arg(format!("{} {}", command, *item))
-                    .output()
+                    .spawn()
                     .expect("failed to execute")
                     .stdout;
             }
