01c0efe121ea03f378e4441c9c78f070ad5aad27
Nick <nicktomthomas@gmail.com>
2025-07-07 02:35:42 -0400

    chore: wip

--- rust-web-development/rust-xargs/src/main.rs
+++ rust-web-development/rust-xargs/src/main.rs
@@ -1,19 +1,49 @@
 use std::{io::Read, process::Command};
 
-use clap::Parser;
+//Step 3
+//
+//In this step your goal is to support the -n option, which does this (from the man page):
+//
+// -n number, --max-args=number
+//             Set the maximum number of arguments taken from standard
+//             input for each invocation of utility.  An invocation of
+//             utility will use less than number standard input arguments
+//             if the number of bytes accumulated (see the -s option)
+//             exceeds the specified size or there are fewer than number
+//             arguments remaining for the last invocation of utility.  The
+//             current default value for number is 5000.
+//
+//You can then test this like so:
+
+//% ls | ccxargs -n 1 cat
+//This is file 1
+//This is file 2
+//This is file 3
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
     /// Optional name to operate on
     command: Option<Vec<String>>,
+
+    #[command(subcommand)]
+    morecommand: Option<MoreCommand>,
+}
+
+#[derive(Subcommand)]
+enum MoreCommand {
+    List {
+        #[arg(short, long)]
+        max_args: Option<u8>,
+    },
 }
 
 fn main() {
+    let cli = Cli::parse();
     let mut buffer = String::new();
     let _ = std::io::stdin().read_to_string(&mut buffer);
     let stdin_input = buffer.split_whitespace().collect::<Vec<&str>>();
-    let cli = Cli::parse();
 
     // You can check the value provided by positional arguments, or option arguments
 
