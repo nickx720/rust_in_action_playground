eef287d7ae3487910054c0e212ab0773c967121f
Nick <nicktomthomas@gmail.com>
2025-07-17 04:17:14 -0400

    feat: adding http server

--- rust-web-development/http-forward-proxy/src/main.rs
+++ rust-web-development/http-forward-proxy/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+//     Have your proxy server start up and listen for connections. In my case I decided to listen on port 8989.
+
+//    When a request is received parse it to extract the target host.
+//
+//    Create a new socket / connection to the target server.
+//
+//    Forward the request, minus the hop by hop headers.
+//
+//    Change the GET request.
+//
+//    Add the ‘X-Forwarded-For’ header.
+//
+//    Read the response from the target server and set the correct response headers before,
+//
+//    Sending the response to the client.
+
+// TODO use tokio
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_client(stream: &mut TcpStream) {
+    let response = b"HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello World!";
+    let _ = stream.write_all(response);
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(&mut stream?);
+    }
+    Ok(())
 }
