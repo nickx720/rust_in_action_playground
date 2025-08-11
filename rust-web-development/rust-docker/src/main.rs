8a13eb440df6614edec9e9ecba933ab517b9c91a
Nick <nicktomthomas@gmail.com>
2025-08-11 03:45:28 -0400

    chore: wip

--- rust-web-development/rust-docker/src/main.rs
+++ rust-web-development/rust-docker/src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    run: Option<Commands>,
+}
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Image {
+        #[arg(short, long)]
+        command: String,
+        #[arg(short, long)]
+        args: Vec<String>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if let Some(arguments) = args.run {
+        dbg!(&arguments);
+    }
 }
