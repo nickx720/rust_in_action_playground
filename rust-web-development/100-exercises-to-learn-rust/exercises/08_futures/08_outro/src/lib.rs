0941654756b8305ca3378463b4e0b6c0dcb6ae85
Nick <nicktomthomas@gmail.com>
2025-09-13 06:39:20 -0400

    chore: adding create method

--- rust-web-development/100-exercises-to-learn-rust/exercises/08_futures/08_outro/src/lib.rs
+++ rust-web-development/100-exercises-to-learn-rust/exercises/08_futures/08_outro/src/lib.rs
@@ -9,21 +9,44 @@
 // Use Rust's package registry, crates.io, to find the dependencies you need
 // (if any) to build this system.
 
+use hyper::body::HttpBody;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use serde::Deserialize;
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use ticket_fields::{TicketDescription, TicketTitle};
+
+use crate::ticket::{Ticket, TicketStore};
 mod ticket;
 
-async fn hello(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello, world!")))
+#[derive(Deserialize, Debug)]
+struct ParseBody {
+    title: String,
+    description: String,
 }
 
+async fn create(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let body = hyper::body::to_bytes(req.into_body()).await.unwrap();
+    let body: ParseBody =
+        serde_json::from_slice::<ParseBody>(&body).expect("Parsing failed, validate");
+    let title = TicketTitle::try_from(body.title);
+    let description = TicketTitle::try_from(body.description);
+    let mut created = Response::new(Body::from("Created"));
+    *created.status_mut() = StatusCode::CREATED;
+    Ok(created)
+}
+async fn read(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Read stub")))
+}
+async fn patch(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Patch stub")))
+}
 async fn router(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     match (req.method(), req.uri().path()) {
-        (&Method::POST, "/create") => hello(req).await,
-        (&Method::GET, "/read") => hello(req).await,
-        (&Method::PATCH, "/update") => hello(req).await,
+        (&Method::POST, "/create") => create(req).await,
+        (&Method::GET, "/read") => read(req).await,
+        (&Method::PATCH, "/update") => patch(req).await,
         _ => {
             let mut not_found = Response::new(Body::from("Not Found"));
             *not_found.status_mut() = StatusCode::NOT_FOUND;
@@ -34,7 +57,8 @@ async fn router(req: Request<Body>) -> Result<Response<Body>, Infallible> {
 
 pub async fn run_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-
+    // TODO pass ticket_store to routes
+    let mut ticket = TicketStore::new();
     let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(router)) });
 
     let server = Server::bind(&addr).serve(make_svc);
