dd587bfd34525dea0ac1fa5d43974f4eceb3f989
Nick <nicktomthomas@gmail.com>
2025-11-18 03:54:30 -0500

    feat: tar extractor

---
+++ rust-web-development/rust-tar-extractor/src/main.rs
@@ -0,0 +1,21 @@
+use std::{fs::File, io::BufReader};
+
+use flate2::bufread::GzDecoder;
+use tar::Archive;
+
+fn main() -> Result<(), anyhow::Error> {
+    let file = File::open("test.tar.gz")?;
+    let buf_reader = BufReader::new(file);
+    let gz = GzDecoder::new(buf_reader);
+    let mut archive = Archive::new(gz);
+    let entries = archive.entries()?;
+    for entry in entries {
+        let entry = entry?;
+        dbg!(entry.path());
+    }
+
+    //    let buf_reader = BufReader::new(file);
+    //    let gz = GzDecoder::new(buf_reader);
+    println!("Hello, world!");
+    Ok(())
+}
