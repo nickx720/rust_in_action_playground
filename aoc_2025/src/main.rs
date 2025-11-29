9388c86e5bda6ae94fdf5dbfe1a039654d94bffe
Nick <nicktomthomas@gmail.com>
2025-11-29 08:36:45 -0500

    chore: adding helper for reading

--- aoc_2025/src/main.rs
+++ aoc_2025/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use aoc_2025::read;
+
+fn main() -> Result<(), anyhow::Error> {
+    let path = "./assets/day_one/sample.txt";
+    let input = read::read(path)?;
+    Ok(())
 }
