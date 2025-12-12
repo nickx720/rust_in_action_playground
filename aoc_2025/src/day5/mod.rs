dc6d2d422eb1bee91943886dd0dae63b1f37b3c9
Nick <nicktomthomas@gmail.com>
2025-12-12 03:28:39 -0500

    chore: wip

--- aoc_2025/src/day5/mod.rs
+++ aoc_2025/src/day5/mod.rs
@@ -1,3 +1,5 @@
+use anyhow::Context;
+
 fn process_input<T>(item: Option<T>) -> Result<Vec<String>, anyhow::Error>
 where
     T: AsRef<str>,
@@ -13,7 +15,26 @@ where
 pub fn dayfive_partone(input: &str) -> Result<usize, anyhow::Error> {
     let input = input.split("\n\n").collect::<Vec<&str>>();
     let mut input = input.iter();
-    let ranges = process_input(input.next())?;
+    let ranges: Vec<(usize, usize)> = process_input(input.next())?
+        .into_iter()
+        .map(|item| {
+            Ok::<Vec<(usize, usize)>, anyhow::Error>(
+                item.split("-")
+                    .map(|value| {
+                        value
+                            .parse::<usize>()
+                            .with_context(|| format!("Conversion failed"))
+                    })
+                    .collect::<Result<Vec<_>, _>>()?
+                    .chunks(2)
+                    .map(|chunk| (chunk[0], chunk[1]))
+                    .collect::<Vec<(usize, usize)>>(),
+            )
+        })
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .flatten()
+        .collect();
     let items = process_input(input.next())?;
     dbg!(ranges, items);
     todo!()
