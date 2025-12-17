435311138b37ae07816e865866cf74fd13f840a5
Nick <nicktomthomas@gmail.com>
2025-12-17 13:04:31 -0500

    feat: day 6 part one completed

--- aoc_2025/src/day6/mod.rs
+++ aoc_2025/src/day6/mod.rs
@@ -1,4 +1,48 @@
+use anyhow::{Context, bail};
+
 pub fn day6_partone(input: &str) -> Result<usize, anyhow::Error> {
-    let input = input;
-    todo!()
+    let input = input
+        .trim()
+        .split("\n")
+        .map(|item| item.trim().split_whitespace().collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>();
+    let rows = input.len();
+    let col = input[0].len();
+    // 0 1 2 3
+    // 1 2 3
+    let mut total = 0usize;
+    for index in 0..col {
+        let mut op = ' ';
+        let mut nums = vec![];
+        for second_index in (0..rows).rev() {
+            if input[second_index][index] == "*" {
+                op = '*';
+                continue;
+            }
+            if input[second_index][index] == "+" {
+                op = '+';
+                continue;
+            }
+            let number = input[second_index][index]
+                .parse::<usize>()
+                .with_context(|| format!("Parsing failed for {}{}", second_index, index))?;
+            nums.push(number);
+        }
+        match op {
+            '+' => {
+                total += nums
+                    .iter()
+                    .skip(1)
+                    .fold(nums[0], |acc, &x| acc.saturating_add(x))
+            }
+            '*' => {
+                total += nums
+                    .iter()
+                    .skip(1)
+                    .fold(nums[0], |acc, &x| acc.saturating_mul(x))
+            }
+            _ => bail!("Whoops invalid op"),
+        }
+    }
+    Ok(total)
 }
--- aoc_2025/src/main.rs
+++ aoc_2025/src/main.rs
@@ -1,9 +1,6 @@
 use std::{env, time::Instant};
 
-use aoc_2025::{
-    day5::{day5_partone, day5_parttwo},
-    load, read,
-};
+use aoc_2025::{day6::day6_partone, load, read};
 
 fn main() -> Result<(), anyhow::Error> {
     let args: Vec<String> = env::args().collect();
@@ -12,20 +9,20 @@ fn main() -> Result<(), anyhow::Error> {
         let path = format!("./assets/day{}/{}.txt", day, day);
         let input = read::read(&path)?;
         let start = Instant::now();
-        let output_one = day5_partone(&input)?;
-        let output_two = day5_parttwo(&input)?;
+        let output_one = day6_partone(&input)?;
+        //let output_two = day5_parttwo(&input)?;
         let elapsed = start.elapsed();
 
         println!("The output for day one is {}", output_one);
-        println!("The output for day two is {}", output_two);
+        // println!("The output for day two is {}", output_two);
         println!("Runtime: {}µs", elapsed.as_micros());
     } else {
         let path = format!("./sample.txt");
         let input = read::read(&path)?;
-        let output = day5_partone(&input)?;
+        let output = day6_partone(&input)?;
         println!("The output for day one is {}", output);
-        let output = day5_parttwo(&input)?;
-        println!("The output for day two is {}", output);
+        //     let output = day5_parttwo(&input)?;
+        //     println!("The output for day two is {}", output);
     }
     Ok(())
 }
