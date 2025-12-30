3cbdcbb6c8c9fcf7530ea77ec9480170984f5d11
Nick <nicktomthomas@gmail.com>
2025-12-30 02:38:04 -0500

    feat: day 8 part one

--- aoc_2025/src/main.rs
+++ aoc_2025/src/main.rs
@@ -1,10 +1,6 @@
 use std::{env, time::Instant};
 
-use aoc_2025::{
-    day7::{day7_partone, day7_parttwo},
-    day8::day8_partone,
-    load, read,
-};
+use aoc_2025::{day8::day8_partone, load, read};
 
 fn main() -> Result<(), anyhow::Error> {
     let args: Vec<String> = env::args().collect();
@@ -13,17 +9,17 @@ fn main() -> Result<(), anyhow::Error> {
         let path = format!("./assets/day{}/{}.txt", day, day);
         let input = read::read(&path)?;
         let start = Instant::now();
-        let output_one = day7_partone(&input)?;
-        let output_two = day7_parttwo(&input)?;
+        let output_one = day8_partone(&input, 1000)?;
+        //        let output_two = day7_parttwo(&input)?;
         let elapsed = start.elapsed();
 
         println!("The output for day one is {}", output_one);
-        println!("The output for day two is {}", output_two);
+        //       println!("The output for day two is {}", output_two);
         println!("Runtime: {}µs", elapsed.as_micros());
     } else {
         let path = format!("./sample.txt");
         let input = read::read(&path)?;
-        let output = day8_partone(&input)?;
+        let output = day8_partone(&input, 10)?;
         println!("The output for day one is {}", output);
         //        let output = day7_parttwo(&input)?;
         //        println!("The output for day two is {}", output);
