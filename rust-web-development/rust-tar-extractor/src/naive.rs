b30f79daf4f611609ac3ed3cb132ad5cbfa4dcce
Nick <nicktomthomas@gmail.com>
2026-01-27 02:38:46 -0500

    feat: rust tar extractor

---
+++ rust-web-development/rust-tar-extractor/src/naive.rs
@@ -0,0 +1,34 @@
+use std::{
+    fs::{self, File},
+    io::BufReader,
+    path::PathBuf,
+};
+
+use flate2::bufread::GzDecoder;
+use tar::{Archive, EntryType};
+pub fn zip() -> Result<(), anyhow::Error> {
+    let file = File::open("test.tar.gz")?;
+    let buf_reader = BufReader::new(file);
+    let gz = GzDecoder::new(buf_reader);
+    let mut archive = Archive::new(gz);
+    let entries = archive.entries()?;
+    let root = PathBuf::from("test");
+    if !fs::exists(&root)? {
+        fs::create_dir(&root)?;
+    }
+    for entry in entries {
+        let mut entry = entry?;
+        let header = entry.header();
+        if header.entry_type() == EntryType::Directory {
+            let mut child_path = root.clone();
+            child_path.push(header.path()?);
+            if !fs::exists(&child_path)? {
+                fs::create_dir(child_path)?;
+            }
+            continue;
+        }
+        entry.unpack_in(&root)?;
+    }
+
+    Ok(())
+}
