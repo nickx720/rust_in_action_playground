90ac76a9ad1c83efbe3ecb0a051148880882bc02
Nick <nicktomthomas@gmail.com>
2026-02-08 09:46:44 -0500

    chore: wip

--- rust-web-development/rust-bloom-filter/src/main.rs
+++ rust-web-development/rust-bloom-filter/src/main.rs
@@ -1,15 +1,22 @@
-use std::fs;
+use std::{env, fs};
 
 use crate::lib::Bloom;
 
 mod lib;
 fn main() {
-    let mut bloom = Bloom::new(100, 0.01);
-    let file = fs::read_to_string("./dict.txt").expect("Reading file failed");
-    for item in file.split("\n") {
-        if item.is_empty() {
-            continue;
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Needs a source of words to build");
+        return;
+    }
+    if let Some(path) = args.get(1) {
+        let mut bloom = Bloom::new(100, 0.01);
+        let file = fs::read_to_string(path).expect("Reading file failed");
+        for item in file.split("\n") {
+            if item.is_empty() {
+                continue;
+            }
+            bloom.insert(item);
         }
-        bloom.insert(item);
     }
 }
