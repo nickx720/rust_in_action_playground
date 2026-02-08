e9de8740a169061a8e08ec2530f3536772e1772c
Nick <nicktomthomas@gmail.com>
2026-02-08 09:39:49 -0500

    chore: step two wip

--- rust-web-development/rust-bloom-filter/src/main.rs
+++ rust-web-development/rust-bloom-filter/src/main.rs
@@ -1,2 +1,15 @@
+use std::fs;
+
+use crate::lib::Bloom;
+
 mod lib;
-fn main() {}
+fn main() {
+    let mut bloom = Bloom::new(100, 0.01);
+    let file = fs::read_to_string("./dict.txt").expect("Reading file failed");
+    for item in file.split("\n") {
+        if item.is_empty() {
+            continue;
+        }
+        bloom.insert(item);
+    }
+}
