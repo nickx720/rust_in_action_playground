8b1af442d1f5a8dba8407486c1336d87c4a96d33
Nick <nicktomthomas@gmail.com>
2026-03-15 09:29:49 -0400

    chore: wip

--- rust-web-development/rust-bloom-filter/src/main.rs
+++ rust-web-development/rust-bloom-filter/src/main.rs
@@ -5,21 +5,25 @@ mod bloom;
 
 use crate::bloom::Bloom;
 
+fn build_word_bf(path: String) -> Result<(), Box<dyn std::error::Error>> {
+    let mut bloom = Bloom::new(100, 0.01);
+    let file = fs::read_to_string(path).expect("Reading file failed");
+    for item in file.split("\n") {
+        if item.is_empty() {
+            continue;
+        }
+        bloom.insert(item);
+    }
+    fs::write("words.bf", bloom.save_to_disk())?;
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
+    if args.len() < 1 {
         return Err("Needs a source of words to build".into());
     }
     if let Some(path) = args.get(1) {
-        let mut bloom = Bloom::new(100, 0.01);
-        let file = fs::read_to_string(path).expect("Reading file failed");
-        for item in file.split("\n") {
-            if item.is_empty() {
-                continue;
-            }
-            bloom.insert(item);
-        }
-        fs::write("words.bf", bloom.save_to_disk())?;
         let file = fs::read("words.bf")?;
         let bloom = Bloom::read_from_disk(file);
         let file = fs::read_to_string(path).expect("Reading file failed");
@@ -28,7 +32,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 continue;
             }
             let output = bloom.exists(item);
-            dbg!(output);
         }
     }
     Ok(())
