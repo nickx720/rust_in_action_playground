4bc9cd2684ef2ca714e56e312899431f60753b0f
Nick <nicktomthomas@gmail.com>
2026-03-21 03:24:06 -0400

    chore: wip

--- rust-web-development/rust-bloom-filter/src/main.rs
+++ rust-web-development/rust-bloom-filter/src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 #[path = "lib/mod.rs"]
 mod bloom;
 
-use crate::bloom::Bloom;
+use crate::bloom::{Bloom, BloomFilter};
 
 fn build_word_bf(path: String) -> Result<(), Box<dyn std::error::Error>> {
     let mut bloom = Bloom::new(100, 0.01);
@@ -20,19 +20,46 @@ fn build_word_bf(path: String) -> Result<(), Box<dyn std::error::Error>> {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 1 {
-        return Err("Needs a source of words to build".into());
-    }
-    if let Some(path) = args.get(1) {
-        let file = fs::read("words.bf")?;
-        let bloom = Bloom::read_from_disk(file);
-        let file = fs::read_to_string(path).expect("Reading file failed");
-        for item in file.split("\n") {
-            if item.is_empty() {
-                continue;
+    let output = match args.as_slice() {
+        [] => Err("Needs a source of words to build".into()),
+        [build, path] if *build == "build".to_string() => build_word_bf(path.clone()),
+        items => {
+            if items.len() == 0 {
+                return Err("Need words".into());
             }
-            let output = bloom.exists(item);
+            let file = fs::read("words.bf")?;
+            let bloom = Bloom::read_from_disk(file);
+            let mut print_output = Vec::new();
+            for item in items {
+                match bloom.exists(item) {
+                    BloomFilter::NotPresent => {
+                        print_output.push(item);
+                    }
+                    _ => continue,
+                }
+            }
+            let output: String = print_output
+                .iter()
+                .map(|&item| item.to_owned())
+                .collect::<Vec<_>>()
+                .join("\n");
+            println!("These words are spelt wrong {}", output);
+            Ok(())
         }
-    }
-    Ok(())
+    };
+    //    if args.len() < 1 {
+    //        return;
+    //    }
+    //    if let Some(path) = args.get(1) {
+    //        let file = fs::read("words.bf")?;
+    //        let bloom = Bloom::read_from_disk(file);
+    //        let file = fs::read_to_string(path).expect("Reading file failed");
+    //        for item in file.split("\n") {
+    //            if item.is_empty() {
+    //                continue;
+    //            }
+    //            let output = bloom.exists(item);
+    //        }
+    //    }
+    output
 }
