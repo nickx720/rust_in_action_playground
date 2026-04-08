c038dd3697d08fbcd26d4c19e493e0e952393119
Nick <nicktomthomas@gmail.com>
2026-04-08 06:11:09 -0400

    chore: wip

--- rust-web-development/rust_compression/src/main.rs
+++ rust-web-development/rust_compression/src/main.rs
@@ -1,12 +1,13 @@
-use std::env;
+use std::{env, fs};
 
-fn valid_file_path(items: impl Iterator<Item = String>) {
+fn valid_file_path(items: impl Iterator<Item = String>) -> Result<(), anyhow::Error> {
     for arg in items {
-        println!("{}", arg);
+        let file = fs::canonicalize(arg)?;
     }
-    todo!()
+    Ok(())
 }
-fn main() {
+fn main() -> Result<(), anyhow::Error> {
     let args = env::args().skip(1);
-    valid_file_path(args);
+    valid_file_path(args)?;
+    Ok(())
 }
