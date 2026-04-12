7bf29cf653cf4c4c3ffbb74e865475956b86aebe
Nick <nicktomthomas@gmail.com>
2026-04-12 19:45:32 -0400

    chore: wip

--- rust-web-development/rust_xxd/src/main.rs
+++ rust-web-development/rust_xxd/src/main.rs
@@ -4,8 +4,8 @@ fn main() -> Result<(), anyhow::Error> {
     let args = std::env::args().skip(1);
     for file in args {
         let file = fs::canonicalize(file)?;
-        let contents = fs::read(file)?;
-        for byte in contents.chunks(16) {
+        let raw_contents = fs::read(file)?;
+        for byte in raw_contents.chunks(16) {
             for pair in byte.chunks(2) {
                 print!("{:02x}{:02x} ", pair[0], pair[1]);
             }
