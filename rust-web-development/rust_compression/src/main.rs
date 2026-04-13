c385c2be9472f25750be71317ae7752e8876cc65
Nick <nicktomthomas@gmail.com>
2026-04-13 19:17:25 -0400

    chore: wip

--- rust-web-development/rust_compression/src/main.rs
+++ rust-web-development/rust_compression/src/main.rs
@@ -1,10 +1,15 @@
 use std::{env, fs};
 
+fn frequency_counter(contents: Vec<u8>) -> Result<(), anyhow::Error> {
+    dbg!(std::str::from_utf8(&contents));
+    todo!();
+}
+
 fn valid_file_path(items: impl Iterator<Item = String>) -> Result<(), anyhow::Error> {
     for arg in items {
         let file = fs::canonicalize(arg)?;
         let contents = fs::read(file)?;
-        dbg!(contents);
+        frequency_counter(contents)?;
     }
     Ok(())
 }
