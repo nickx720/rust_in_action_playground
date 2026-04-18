3ba2669329b0f31ef2e8a3da6d0e6b2ee6c8aefa
Nick <nicktomthomas@gmail.com>
2026-04-18 11:46:47 -0400

    chore: wip

--- rust-web-development/rust_compression/src/main.rs
+++ rust-web-development/rust_compression/src/main.rs
@@ -7,8 +7,11 @@ use std::{
 
 fn frequency_counter(data: &[u8]) -> Result<(), anyhow::Error> {
     let string_to_count = String::from_utf8_lossy(data);
-    let map = HashMap::new();
+    let mut map = HashMap::new();
     for word in string_to_count.trim().split("") {
+        map.entry(word)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
         dbg!(word);
     }
     todo!();
