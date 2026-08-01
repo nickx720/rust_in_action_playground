c45a8a97366a2142720a06753bcfa29b8cb69d17
Nick <nicktomthomas@gmail.com>
2026-08-01 04:15:01 -0400

    chore: wip

--- rust-web-development/rust_chess/src/moves.rs
+++ rust-web-development/rust_chess/src/moves.rs
@@ -1,10 +1,24 @@
 use crate::{board::Board, chess::Square};
-
+// codex resume 019fbc54-c741-78b2-a33e-90ab75d843f2
 impl Board {
-    pub fn pseudo_legal_moves(self, square: Square) {
+    pub fn pseudo_legal_moves(&self, square: Square) -> Vec<Square> {
         todo!()
     }
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+
+    #[test]
+    fn psuedo_legal_moves_accepts_middle_knight_moves() {
+        todo!()
+    }
+    #[test]
+    fn psuedo_legal_moves_accepts_corner_knight_moves() {
+        todo!()
+    }
+    #[test]
+    fn psuedo_legal_moves_accepts_corner_knight_moves_with_opponents() {
+        todo!()
+    }
+}
