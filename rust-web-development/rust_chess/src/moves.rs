ec532ef370fb53ffa8fbb55513e5e5f3a041c899
Nick <nicktomthomas@gmail.com>
2026-08-05 05:27:33 -0400

    chore: wip

--- rust-web-development/rust_chess/src/moves.rs
+++ rust-web-development/rust_chess/src/moves.rs
@@ -2,6 +2,15 @@ use crate::{
     board::Board,
     chess::{ChessMove, Piece, Square},
 };
+
+impl Square {
+    pub fn is_corner(self) -> bool {
+        if (self.file == 0 || self.file == 7) && (self.rank == 0 || self.rank == 7) {
+            return true;
+        }
+        false
+    }
+}
 impl Board {
     // codex resume 019fbc54-c741-78b2-a33e-90ab75d843f2
     pub fn get(&self, square: Square) -> Option<Piece> {
@@ -10,7 +19,14 @@ impl Board {
     pub fn pseudo_legal_moves(&self, square: Square) -> Option<Vec<ChessMove>> {
         if let Some(piece) = &self.get(square) {
             match piece.kind {
-                crate::chess::PieceKind::Knight => Some(vec![]),
+                crate::chess::PieceKind::Knight => {
+                    // corners only two directions
+                    // middle of the board can go in 4 direction
+                    if square.is_corner() {
+                        // corner
+                    }
+                    Some(vec![])
+                }
                 _ => None,
             }
         } else {
