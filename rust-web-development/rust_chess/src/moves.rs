5a32ed6f5e316cdb599aa3a849fd1d8a5a0eb432
Nick <nicktomthomas@gmail.com>
2026-08-10 02:54:45 -0400

    chore: wip

--- rust-web-development/rust_chess/src/moves.rs
+++ rust-web-development/rust_chess/src/moves.rs
@@ -14,7 +14,7 @@ impl Square {
 impl Board {
     // codex resume 019fbc54-c741-78b2-a33e-90ab75d843f2
     pub fn get(&self, square: Square) -> Option<Piece> {
-        self.board[square.file as usize][square.rank as usize]
+        self.board[square.rank as usize][square.file as usize]
     }
     pub fn pseudo_legal_moves(&self, square: Square) -> Option<Vec<ChessMove>> {
         if let Some(piece) = &self.get(square) {
@@ -42,14 +42,23 @@ impl Board {
                         // middle of the board can go in 4 direction
                     }
                     let square_top_left = Square::new(square.file + 2, square.rank + 1);
+                    let square_top_right = Square::new(square.file + 2, square.rank - 1);
                     let square_bottom_left = Square::new(square.file - 2, square.rank - 1);
-                    let square_top_right = Square::new(square.file + 1, square.rank + 2);
-                    let square_bottom_right = Square::new(square.file - 1, square.rank - 2);
+                    let square_bottom_right = Square::new(square.file - 2, square.rank - 1);
+                    let square_left_top = Square::new(square.file - 1, square.rank + 2);
+                    let square_left_bottom = Square::new(square.file - 1, square.rank - 2);
+                    let square_right_top = Square::new(square.file + 1, square.rank + 2);
+                    let square_right_bottom = Square::new(square.file + 1, square.rank - 2);
+                    // TODO src/chess.rs:49 compares only two of the four move coordinates, so different moves may compare equal.
                     possible_moves.extend(vec![
                         ChessMove::new(square, square_top_left),
                         ChessMove::new(square, square_top_right),
                         ChessMove::new(square, square_bottom_left),
                         ChessMove::new(square, square_bottom_right),
+                        ChessMove::new(square, square_left_top),
+                        ChessMove::new(square, square_left_bottom),
+                        ChessMove::new(square, square_right_top),
+                        ChessMove::new(square, square_right_bottom),
                     ]);
                     Some(possible_moves)
                 }
