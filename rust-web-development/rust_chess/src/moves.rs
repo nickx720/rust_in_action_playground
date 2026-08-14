b99571169bf56c6f8c10cca5e439867cb82d94d6
Nick <nicktomthomas@gmail.com>
2026-08-14 08:37:03 -0400

    feat: all tests for knight pass,refactor

--- rust-web-development/rust_chess/src/moves.rs
+++ rust-web-development/rust_chess/src/moves.rs
@@ -3,14 +3,6 @@ use crate::{
     chess::{ChessMove, Piece, Square},
 };
 
-impl Square {
-    pub fn is_corner(self) -> bool {
-        if (self.file == 0 || self.file == 7) && (self.rank == 0 || self.rank == 7) {
-            return true;
-        }
-        false
-    }
-}
 impl Board {
     // codex resume 019fbc54-c741-78b2-a33e-90ab75d843f2
     pub fn get(&self, square: Square) -> Option<Piece> {
@@ -21,43 +13,28 @@ impl Board {
             match piece.kind {
                 crate::chess::PieceKind::Knight => {
                     let mut possible_moves = vec![];
-                    if square.is_corner() {
-                        // corners only two directions
-                        if square.file == 0 {
-                            let square_to = Square::new(square.file + 2, square.rank + 1);
-                            let square_to_another = Square::new(square.file + 1, square.rank + 2);
-                            possible_moves.extend(vec![
-                                ChessMove::new(square, square_to),
-                                ChessMove::new(square, square_to_another),
-                            ]);
-                        } else {
-                            let square_to = Square::new(square.file - 2, square.rank - 1);
-                            let square_to_another = Square::new(square.file - 1, square.rank - 2);
-                            possible_moves.extend(vec![
-                                ChessMove::new(square, square_to),
-                                ChessMove::new(square, square_to_another),
-                            ]);
+                    let offsets = [
+                        (2, 1),
+                        (2, -1),
+                        (-2, 1),
+                        (-2, -1),
+                        (1, 2),
+                        (1, -2),
+                        (-1, 2),
+                        (-1, -2),
+                    ];
+
+                    for (file_offset, rank_offset) in offsets {
+                        let Some(file) = square.file.checked_add_signed(file_offset) else {
+                            continue;
+                        };
+                        let Some(rank) = square.rank.checked_add_signed(rank_offset) else {
+                            continue;
+                        };
+
+                        if file < 8 && rank < 8 {
+                            possible_moves.push(ChessMove::new(square, Square::new(file, rank)));
                         }
-                    } else {
-                        // middle of the board can go in 4 direction
-                        let square_top_left = Square::new(square.file + 2, square.rank + 1);
-                        let square_top_right = Square::new(square.file + 2, square.rank - 1);
-                        let square_bottom_left = Square::new(square.file - 2, square.rank - 1);
-                        let square_bottom_right = Square::new(square.file - 2, square.rank - 1);
-                        let square_left_top = Square::new(square.file - 1, square.rank + 2);
-                        let square_left_bottom = Square::new(square.file - 1, square.rank - 2);
-                        let square_right_top = Square::new(square.file + 1, square.rank + 2);
-                        let square_right_bottom = Square::new(square.file + 1, square.rank - 2);
-                        possible_moves.extend(vec![
-                            ChessMove::new(square, square_top_left),
-                            ChessMove::new(square, square_top_right),
-                            ChessMove::new(square, square_bottom_left),
-                            ChessMove::new(square, square_bottom_right),
-                            ChessMove::new(square, square_left_top),
-                            ChessMove::new(square, square_left_bottom),
-                            ChessMove::new(square, square_right_top),
-                            ChessMove::new(square, square_right_bottom),
-                        ]);
                     }
                     Some(possible_moves)
                 }
