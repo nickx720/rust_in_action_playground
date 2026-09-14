21af3e70bb6dd158c19ac8c44328d17e369ac808
Nick <nicktomthomas@gmail.com>
2026-09-14 03:05:44 -0400

    chore: wip

--- rust-web-development/rust_chess/src/moves.rs
+++ rust-web-development/rust_chess/src/moves.rs
@@ -135,7 +135,10 @@ impl Board {
                             let pos_square = Square::new(next_file, next_rank);
                             match self.get(pos_square) {
                                 Some(piece_at_position) => {
-                                    todo!()
+                                    if piece_at_position.color != piece.color {
+                                        possible_moves.push(ChessMove::new(square, pos_square));
+                                    }
+                                    break;
                                 }
                                 None => possible_moves.push(ChessMove::new(square, pos_square)),
                             }
